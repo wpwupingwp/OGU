ee9e395a35b34423203d4a01102b76164ebe1204
wpwupingwp <wpwupingwp@hotmail.com>
2014-02-16 17:38:58 +0800

    FileIO

--- script.py
+++ script.py
@@ -1,10 +1,11 @@
 from Bio import SeqIO
-#File=input("File name:\n")
-File="gb"
-record=SeqIO.read(File,"genbank")
-print(record.id)
-exon=[]
-for i in record.features:
+#FileIn=input("File name:\n")
+FileIn="gb"
+Record=SeqIO.read(FileIn,"genbank")
+Organism=Record.annotations["organism"].replace(" ","_")
+Accession=Record.annotations["accessions"][0]
+Exon=[]
+for i in Record.features:
     if i.type=="gene" and "gene" in i.qualifiers:
         if i.location_operator!="join":
             #Ignore CompoundLocation
@@ -12,20 +13,29 @@ for i in record.features:
             Start=int(i.location.start)
             End=int(i.location.end)
             Strand=int(i.location.strand)
-            rec=[GeneName,Start,End,Strand]
-            exon.append(rec)
+            Sequence=str(Record.seq[Start:End])
+            rec=[GeneName,Start,End,Strand,Sequence]
+            Exon.append(rec)
         elif i.location_operator=="join":
             print(i.qualifiers["gene"],i.location)
-intron=[]
-for i in range(len(exon)-1):
+Intron=[]
+for i in range(len(Exon)-1):
     #Ignore ChongDieGene
-    This=exon[i]
-    Next=exon[i+1]
+    This=Exon[i]
+    Next=Exon[i+1]
     IntronName="-".join([This[0][2:-2],Next[0][2:-2]])
     Start=This[2]+1
     End=Next[1]-1
-    print(IntronName,Start,End)
-
+    Sequence=str(Record.seq[Start:End])
+    rec=[IntronName,Start,End,Sequence]
+    Intron.append(rec)
+FileOut=open("Out","w")
+for i in Exon:
+    FileOut.write(">%s|%s|%s\n"%(Organism,i[0],Accession))
+    FileOut.write("%s\n"%(i[4]))
+for i in Intron:
+    FileOut.write(">%s|%s|%s\n"%(Organism,i[0],Accession))
+    FileOut.write("%s\n"%(i[3]))
 
     
 
