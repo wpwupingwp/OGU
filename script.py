2a7c3de758b7c2f246b6baa9fb5acf0b01e16c80
wpwupingwp <wpwupingwp@hotmail.com>
2014-02-16 16:00:27 +0800

    reinit

--- script.py
+++ script.py
@@ -3,9 +3,33 @@ from Bio import SeqIO
 File="gb"
 record=SeqIO.read(File,"genbank")
 print(record.id)
+exon=[]
 for i in record.features:
-    if i.type=="gene":
-        if "gene" in i.qualifiers:
+    if i.type=="gene" and "gene" in i.qualifiers:
+        if i.location_operator!="join":
+            #Ignore CompoundLocation
+            GeneName=str(i.qualifiers["gene"])
+            Start=int(i.location.start)
+            End=int(i.location.end)
+            Strand=int(i.location.strand)
+            rec=[GeneName,Start,End,Strand]
+            exon.append(rec)
+        elif i.location_operator=="join":
             print(i.qualifiers["gene"],i.location)
-    elif i.type=="intron":
-        print(i.location,"intron")
+intron=[]
+for i in range(len(exon)-1):
+    #Ignore ChongDieGene
+    This=exon[i]
+    Next=exon[i+1]
+    IntronName="-".join([This[0][2:-2],Next[0][2:-2]])
+    Start=This[2]+1
+    End=Next[1]-1
+    print(IntronName,Start,End)
+
+
+    
+
+
+
+
+
