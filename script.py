268bcb31947ae8a599a0703d072b5b815b9b6692
wpwupingwp <wpwupingwp@hotmail.com>
2014-02-16 17:51:58 +0800

    Fix name

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ for i in Record.features:
     if i.type=="gene" and "gene" in i.qualifiers:
         if i.location_operator!="join":
             #Ignore CompoundLocation
-            GeneName=str(i.qualifiers["gene"])
+            GeneName=str(i.qualifiers["gene"][0])
             Start=int(i.location.start)
             End=int(i.location.end)
             Strand=int(i.location.strand)
@@ -23,7 +23,7 @@ for i in range(len(Exon)-1):
     #Ignore ChongDieGene
     This=Exon[i]
     Next=Exon[i+1]
-    IntronName="-".join([This[0][2:-2],Next[0][2:-2]])
+    IntronName="-".join([This[0],Next[0]])
     Start=This[2]+1
     End=Next[1]-1
     Sequence=str(Record.seq[Start:End])
@@ -34,12 +34,6 @@ for i in Exon:
     FileOut.write(">%s|%s|%s\n"%(Organism,i[0],Accession))
     FileOut.write("%s\n"%(i[4]))
 for i in Intron:
-    FileOut.write(">%s|%s|%s\n"%(Organism,i[0],Accession))
+    FileOut.write(">%s|%s_Intron|%s\n"%(Organism,i[0],Accession))
     FileOut.write("%s\n"%(i[3]))
-
-    
-
-
-
-
-
+FileOut.write(">%s|Complete Sequence\n%s"%(Organism,str(Record.seq)))
