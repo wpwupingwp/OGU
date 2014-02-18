4601239ea582d1f220de3e121672546ef2230894
wpwupingwp <wpwupingwp@hotmail.com>
2014-02-18 16:52:34 +0800

    Rough version done

--- script.py
+++ script.py
@@ -7,17 +7,20 @@ Accession=Record.annotations["accessions"][0]
 Exon=[]
 for i in Record.features:
     if i.type=="gene" and "gene" in i.qualifiers:
+        #Ignore genes without name
         if i.location_operator!="join":
-            Overlap=0
-            Start=int(i.location.start)
-            End=int(i.location.end)
-            Sequence=str(Record.seq[Start:End])
-            rec=[GeneName,Overlap,Start,End,Strand,Sequence]
+            Join=0
+            Start=[int(i.location.start)]
+            End=[int(i.location.end)]
+            Start.append("-1")
+            End.append("-1")
+            Sequence=str(Record.seq[Start[0]:End[0]])
             GeneName=str(i.qualifiers["gene"][0])
             Strand=int(i.location.strand)
+            rec=[GeneName,Join,Start,End,Strand,Sequence]
             Exon.append(rec)
         elif i.location_operator=="join":
-            Overlap=1
+            Join=1
             Start=[int(i.sub_features[0].location.start)]
             End=[int(i.sub_features[0].location.end)]
             Start.append(int(i.sub_features[1].location.start))
@@ -25,27 +28,23 @@ for i in Record.features:
             GeneName=str(i.qualifiers["gene"][0])
             Strand=int(i.location.strand)
             Sequence="".join([str(Record.seq[Start[0]:End[0]]),str(Record.seq[Start[1]:End[1]])])
-            rec=[GeneName,Overlap,Start,End,Strand,Sequence]
-            GeneName=str(i.qualifiers["gene"][0])
-            Strand=int(i.location.strand)
+            rec=[GeneName,Join,Start,End,Strand,Sequence]
             Exon.append(rec)
 Intron=[]
 for i in range(len(Exon)-1):
-    #Ignore ChongDieGene
+    #Ignore Overlap gene 
     This=Exon[i]
     Next=Exon[i+1]
-    if This[1]==1:
-        pass
-    else:
-        IntronName="-".join([This[0],Next[0]])
-        Start=This[2]+1
-        End=Next[1]-1
-        Sequence=str(Record.seq[Start:End])
-        rec=[IntronName,Start,End,Sequence]
-        Intron.append(rec)
-FileOut=open("Out","w")
+    IntronName="-".join([This[0],Next[0]])
+    Start=This[2][0]+1
+    End=Next[3][0]-1
+    Sequence=str(Record.seq[Start:End])
+    rec=[IntronName,Start,End,Sequence]
+    Intron.append(rec)
+FileOut=open("out","w")
 for i in Exon:
-    FileOut.write(">%s|%s|%s\n"%(Organism,i[0],Accession))
+#    print(i)
+    FileOut.write(">%s|%s|%s|\n"%(Organism,i[0],Accession,))
     FileOut.write("%s\n"%(i[5]))
 for i in Intron:
     FileOut.write(">%s|%s_Intron|%s\n"%(Organism,i[0],Accession))
