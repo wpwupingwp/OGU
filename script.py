ff4f0cc5126afc0ed0824d5a6901df59999304c6
wpwupingwp <wpwupingwp@hotmail.com>
2014-02-21 17:27:39 +0800

    Write as csv file

--- script.py
+++ script.py
@@ -1,52 +1,58 @@
 from Bio import SeqIO
-from Bio import Entrez
+import csv
 def main():
 #    FileIn="gb"
 #    Record=SeqIO.read(FileIn,"genbank")
-    Organism=Record.annotations["organism"].replace(" ","_")
+    Taxon=Record.features[0].qualifiers["db_xref"][0].replace("taxon:","")
+    Organism=Record.annotations["organism"]
     Accession=Record.annotations["accessions"][0]
     Gene=[]
     All=[]
     for i in Record.features:
         if i.type=="gene" and "gene" in i.qualifiers:
             if i.location_operator!="join":
+                Type="gene"
                 Start=int(i.location.start)
                 End=int(i.location.end)
                 Sequence=str(Record.seq[Start:End])
                 Name=str(i.qualifiers["gene"][0])
                 Strand=int(i.location.strand)
-                rec=[Name,Start,End,Strand,Sequence]
+                rec=[Taxon,Organism,Accession,Name,Type,Start,End,Strand,Sequence]
                 Gene.append(rec)
             elif i.location_operator=="join":
+                Type="gene"
                 Start=int(i.sub_features[0].location.start)
                 End=int(i.sub_features[0].location.end)
                 Name=str(i.qualifiers["gene"][0])
                 Strand=int(i.location.strand)
                 Sequence=""
-                rec=[Name,Start,End,Strand,Sequence]
+                rec=[Taxon,Organism,Accession,Name,Type,Start,End,Strand,Sequence]
                 Gene.append(rec)
                 Start=int(i.sub_features[1].location.start)
                 End=int(i.sub_features[1].location.end)
                 Sequence="".join([str(Record.seq[Start:End]),str(Record.seq[Start:End])])
-                rec=[Name,Start,End,Strand,Sequence]
+                rec=[Taxon,Organism,Accession,Name,Type,Start,End,Strand,Sequence]
                 Gene.append(rec)
         elif i.type=="rRNA":
+            Type="rRNA"
             Start=int(i.location.start)
             End=int(i.location.end)
             Sequence=str(Record.seq[Start:End])
             Name=str(i.qualifiers["product"][0]).replace(" ","_")
             Strand=int(i.location.strand)
-            rec=[Name,Start,End,Strand,Sequence]
+            rec=[Taxon,Organism,Accession,Name,Type,Start,End,Strand,Sequence]
             All.append(rec)
         elif i.type=="exon":
+            Type="exon"
             Start=int(i.location.start)
             End=int(i.location.end)
             Sequence=str(Record.seq[Start:End])
             Name="_".join([str(i.qualifiers["gene"][0]),"exon",str(i.qualifiers["number"][0])])
             Strand=int(i.location.strand)
-            rec=[Name,Start,End,Strand,Sequence]
+            rec=[Taxon,Organism,Accession,Name,Type,Start,End,Strand,Sequence]
             All.append(rec)
         elif i.type=="intron":
+            Type="intron"
             Start=int(i.location.start)
             End=int(i.location.end)
             Sequence=str(Record.seq[Start:End])
@@ -55,31 +61,42 @@ def main():
                 Name="_".join([str(i.qualifiers["gene"][0]),"intron",str(i.qualifiers["number"][0])])
             else:
                 Name="_".join([str(i.qualifiers["gene"][0]),"intron"])
-            rec=[Name,Start,End,Strand,Sequence]
+            rec=[Taxon,Organism,Accession,Name,Type,Start,End,Strand,Sequence]
             All.append(rec)
-    Gene.sort(key=lambda x:x[1])
+    Gene.sort(key=lambda x:x[5])
     for i in range(len(Gene)-1):
+        Type="spacer"
         This=Gene[i]
         Next=Gene[i+1]
-        Tail=This[2]+1
-        Head=Next[1]-1
+        Tail=This[6]+1
+        Head=Next[5]-1
         Sequence=str(Record.seq[Tail:Head])
-        Name="_".join(["-".join([This[0],Next[0]]),"Spacer"])
-        Strand=This[3]
-        rec=[Name,Tail,Head,Strand,Sequence]
+        Name="_".join(["-".join([This[3],Next[3]]),"Spacer"])
+        Strand=This[7]
+        rec=[Taxon,Organism,Accession,Name,Type,Start,End,Strand,Sequence]
         All.append(rec)
     All.extend(Gene)
-    All.sort(key=lambda x:x[1])
+    All.sort(key=lambda x:x[5])
+    Database.extend(All)
     FileOut=open(".".join([Accession,"fasta"]),"w")
     for i in All:
-        if i[4]!="":
-            FileOut.write(">%s|%s|%s|\n"%(Organism,i[0],Accession,))
-            FileOut.write("%s\n"%(i[4]))
+        if i[8]!="":
+            FileOut.write(">%s|%s|%s|\n"%(Organism,i[3],Accession,))
+            FileOut.write("%s\n"%(i[8]))
     FileOut.write(">%s|Complete Sequence\n%s"%(Organism,str(Record.seq)))
     return 
 
 
+Database=[]
 FileIn=input("File name:\n")
 Records=list(SeqIO.parse(FileIn,"genbank"))
 for Record in Records:
     main()
+
+csvfile=open("Data.csv","w")
+writer=csv.writer(csvfile)
+writer.writerow(["Taxon","Organism","Accession","Name","Type","Start","End","Strand","Sequence"])
+for item in Database:
+    if item[8]!="":
+        writer.writerow(item)
+csvfile.close()
