eaa18af562ff13132441714d48cfefd39e05a17e
wuping <wpwupingwp@hotmail.com>
2014-07-12 17:47:42 +0800

    fix reverse

--- reverse.py
+++ reverse.py
@@ -1,7 +1,8 @@
-from Bio import SeqRecord,SeqIO
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
 import sys
 Records=list(SeqIO.parse(sys.argv[1],"fasta"))
-handle=open("trim.fasta","a")
+handle=open("reverse_complement.fasta","a")
 for item in Records:
     record=SeqRecord(id=item.id,description="",seq=item.seq.reverse_complement())
-    SeqIO.write(record,"fasta")
+    SeqIO.write(record,handle,"fasta")
