3b4d70976c7787ea8d10da893e1acce176f000dc
wuping <wpwupingwp@hotmail.com>
2014-07-12 16:29:17 +0800

    update remove adapter

--- removeAdapter.py
+++ removeAdapter.py
@@ -1,7 +1,8 @@
 import re
-In=input("filename")
+In=input("filename:\n")
+Adapter=input("adapter:\n")
 handle=open(In,"r")
 Input=str(handle.read())
-out=re.sub("GACTACGCGTCTAGT","",Input)
+out=re.sub(Adapter,"",Input)
 handle=open("Out","w")
 handle.write(out)
