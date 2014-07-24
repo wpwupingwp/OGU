3cf0a9d3c0619f3a6f2a3e666fe1a11b72c2274d
wuping <wpwupingwp@hotmail.com>
2014-07-24 08:29:48 +0800

    sync

--- taxon.py
+++ taxon.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
+import csv
 Id=dict()
 Rank=dict()
 List=[]
+Data=[]
 with open('id','r') as In:
     Raw=list(In.readlines())
     for record in Raw:
@@ -14,5 +16,15 @@ with open('rank','r') as In:
         add=record.replace('\n','').split(sep=' ')
         Rank[add[0]]=add[1]
         if add[1]=='species':
-            List.append(add)
-print(List)
+            List.append(add[0])
+for species in List:
+    record=[species,]
+    while Id[species]!='1':
+        record.append(Id[species])
+        species=Id[species]
+    Data.append(record)
+
+writer=csv.writer(open('out.csv','w',newline=''))
+for item in Data:
+    if '33090' in item:
+        writer.writerow(item)
