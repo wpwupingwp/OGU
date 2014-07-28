d28818254f1811d5fbfb31e5ea157bde5334120c
wuping <wpwupingwp@hotmail.com>
2014-07-28 16:16:43 +0800

    merge taxon.py portable.py

--- portable.py
+++ portable.py
@@ -2,7 +2,12 @@
 
 from Bio import Entrez,SeqIO,BiopythonDeprecationWarning
 from Bio.Seq import MutableSeq
-import sqlite3,warnings,datetime,urllib.request,re
+import datetime
+import re
+import sqlite3
+import urllib.request
+import warnings
+
 warnings.simplefilter("ignore",BiopythonDeprecationWarning)
 
 def Parser():
@@ -19,6 +24,8 @@ def Parser():
     Strand=1
     rec=[Taxon,Organism,Accession,Name,Type,Start,End,Strand,Sequence,Date]
     All.append(rec)
+    global SeqDB
+    SeqDB=[]
     for i in Record.features:
         if i.type=="gene" and "gene" in i.qualifiers:
             if i.location_operator!="join":
@@ -97,6 +104,7 @@ def Parser():
             rec=[Taxon,Organism,Accession,Name,Type,Start,End,Strand,Sequence,Date]
             All.append(rec)
     Gene.sort(key=lambda x:x[5])
+
     for i in range(len(Gene)-1):
         Type="spacer"
         This=Gene[i]
@@ -109,15 +117,15 @@ def Parser():
         rec=[Taxon,Organism,Accession,Name,Type,Start,End,Strand,Sequence,Date]
         All.append(rec)
     All.extend(Gene)
-    Database.extend(All)
-    return 
 
+    SeqDB.extend(All)
+    return 
 
 def InitSeq():
     con=sqlite3.connect("./test/DB")
     cur=con.cursor()
     cur.execute("create table if not exists main (Taxon int,Organism text,Accession text,Name text,Type text,Head int,Tail int, Strand text,Sequence text,Date text,ID integer PRIMARY KEY);")
-    for row in Database:
+    for row in SeqDB:
         if row[8]!="":
             cur.execute("insert into main (Taxon,Organism,Accession,Name,Type,Head,Tail,Strand,Sequence,Date) values (?,?,?,?,?,?,?,?,?,?);",(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9]))
     con.commit()
@@ -179,7 +187,7 @@ def RunSeqQuery(Querytype):
     print("Done.\n")
     return 
 
-def UpdateFromGenbank():
+def UpdateSeqDBFromGenbank():
     Down=urllib.request.urlopen("http://www.ncbi.nlm.nih.gov/genomes/GenomesGroup.cgi?taxid=2759&opt=plastid").read().decode("utf-8")
     GenomeList=re.findall("((?<=nuccore/)[0-9]{9})",Down)
     Entrez.email="wpwupingwp@outlook.com"
@@ -190,9 +198,9 @@ def UpdateFromGenbank():
     Output=open("genbank","w")
     Output.write(GenomeContent.read())
     Output.close()
-    UpdateFromFile("genbank")
+    UpdateSeqFromFile("genbank")
     
-def UpdateFromFile(FileIn):
+def UpdateSeqFromFile(FileIn):
     global Record
     handle=open(FileIn,"r")
     Records=SeqIO.parse(FileIn,"genbank")
@@ -202,17 +210,140 @@ def UpdateFromFile(FileIn):
     handle.close()
     return
 
+def InitTaxon():
+    Id=dict()
+    Data=list()
+    Name=dict()
+    Specie=list()
+    Son=dict()
+    GreatSon=dict()
+    Parent=dict()
+    Rank=dict()
+    global Taxon
+    Taxon=list()
+    with open('./test/name','r') as In:
+        Raw=list(In.readlines())
+        for record in Raw:
+            add=record.replace('\n','').split(sep='|')
+            if add[0] not in Name or add[2]=='scientific name':
+                Name[add[0]]=add[1]
+    with open('./test/nodes','r') as In:
+        Raw=list(In.readlines())
+        for record in Raw:
+            add=record.replace('\n','').split(sep=' ')
+            Id[add[0]]=add[1]
+            Rank[add[0]]=add[2]
+            if add[2]=='species':
+                Specie.append(add[0])
+    for specie in Specie:
+        record=[specie,]
+        while Id[specie]!='1' :
+            record.append(Id[specie])
+            specie=Id[specie]
+#        if '33090' in record:
+#            record.pop()
+#            record.pop()
+            Data.append(record)
+    for data in Data:
+        for n in range(len(data)):
+            if data[n] not in Parent:
+                Parent[data[n]]=data[(n+1):]
+            if n==0:
+                continue
+            if data[n] not in Son:
+                Son[data[n]]={data[n-1],}
+            else:
+                Son[data[n]].add(data[n-1])
+            if data[n] not in GreatSon:
+                GreatSon[data[n]]={data[0],}
+            else:
+                GreatSon[data[n]].add(data[0])
+    for specie in Name.items():
+        if specie[0] not in Son:
+            Son[specie[0]]=set()
+        if specie[0] not in Parent:
+            Parent[specie[0]]=list()
+        if specie[0] not in GreatSon:
+            GreatSon[specie[0]]=set()
+        record=[specie[0],Name[specie[0]],Rank[specie[0]],Son[specie[0]],Parent[specie[0]],GreatSon[specie[0]]]
+        Taxon.append(record)
+
+    con=sqlite3.connect('./test/DB')
+    cur=con.cursor()
+    cur.execute('create table if not exists taxon (Id text,Name text,Rank text,Son text,Parent text,GreatSon text);')
+    for line in Taxon:
+        Son=' '.join(line[3])
+        Parent=' '.join(line[4])
+        GreatSon=' '.join(line[5])
+        cur.execute('insert into taxon (Id,Name,Rank,Son,Parent,GreatSon) values (?,?,?,?,?,?);',(line[0],line[1],line[2],Son,Parent,GreatSon))
+    con.commit()
+    cur.close()
+    con.close()
+    print('Done.\n')
+    return
+    
+def TaxonQueryAuto(Id,Rank):
+    con=sqlite3.connect('./test/DB')
+    cur=con.cursor()
+    cur.execute('select Parent from taxon where Id=? or Name=?;',(Id,'%'+Name+'%'))
+    Result=cur.fetchall()
+    '''to be contiuned'''
+
+def TaxonQueryNoAuto():
+    while True:
+        Querytype=input('1.by id\n2.by name\n')
+        if Querytype not in ['1','2']:
+            return
+        con=sqlite3.connect('./test/DB')
+        cur=con.cursor()
+        if Querytype=='1':
+            Id=input('taxon id:\n')
+            cur.execute('select * from taxon where Id=?;',(Id,))
+            Result=cur.fetchall()
+        elif Querytype=='2':
+            Name=input('scientific name:\n')
+            cur.execute('select * from taxon where Name like ?;',('%'+Name+'%',))
+            Result=cur.fetchall()
+        cur.execute('select Id,Name from taxon;')
+        Result2=cur.fetchall()
+        cur.close()
+        con.close()
+        Namedict={'':''}
+        for item in Result2:
+            Namedict[item[0]]=item[1]
+        for i in Result:
+            Id=i[0]
+            Name=i[1]
+            Rank=i[2]
+            Son=i[3].split(sep=' ')
+            Sonname=list()
+            for item in Son:
+                Sonname.append(Namedict[item])
+            Parent=i[4].split(sep=' ')
+            Parentname=list()
+            for item2 in Parent:
+                Parentname.append(Namedict[item2])
+            GreatSon=i[5].split(sep=' ')
+            GreatSonname=list()
+            for item3 in GreatSon:
+                GreatSonname.append(Namedict[item3])
+            handle=open('out.txt','a',encoding='utf-8')
+            handle.write(''.join(['id      : ',Id,'\n']))
+            handle.write(''.join(['name    : ',Name,'\n']))
+            handle.write(''.join(['rank    : ',Rank,'\n']))
+            handle.write(''.join(['parent  : ','->'.join(Parentname),'\n']))
+            handle.write(''.join(['son     : ',','.join(Sonname),'\n']))
+            handle.write(''.join(['greatson: ',','.join(GreatSonname),'\n\n']))
+
 #Main program 
 Option=input("Select:\n1.Update database from GenBank\n2.Add pvirate data\n3.Query\n")
-Database=[]
 Date=str(datetime.datetime.now())[:19].replace(" ","-")
 if Option=="1":
-    UpdateFromGenbank()
+    UpdateSeqFromGenbank()
 elif Option=="2":
     FileIn=input("Genbank format filename:\n")
-    UpdateFromFile(FileIn)
+    UpdateSeqFromFile(FileIn)
 elif Option=="3":
     SeqQuery()
 else:
     print("Input error!\n")
-
