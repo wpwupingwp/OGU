3d129bddd874c76ae3ce9984d272074939c5dd3c
wuping <wpwupingwp@hotmail.com>
2014-07-28 10:10:22 +0800

    db seems ok

--- taxon.py
+++ taxon.py
@@ -4,13 +4,14 @@ import pickle
 
 def Create():
     Id=dict()
+    Data=list()
     Name=dict()
     Specie=list()
-    Data=list()
     Son=dict()
     Parent=dict()
-    global ToDB
-    ToDB=list()
+    Rank=dict()
+    global Taxon
+    Taxon=list()
     with open('./test/name','r') as In:
         Raw=list(In.readlines())
         for record in Raw:
@@ -22,7 +23,7 @@ def Create():
         for record in Raw:
             add=record.replace('\n','').split(sep=' ')
             Id[add[0]]=add[1]
-            #id,rank,name
+            Rank[add[0]]=add[2]
             if add[2]=='species':
                 Specie.append(add[0])
     for specie in Specie:
@@ -35,26 +36,32 @@ def Create():
             record.pop()
             Data.append(record)
     for data in Data:
-        Parent[data[0]]=set(data[1:])
+        Parent[data[0]]=data[1:]
         for n in range(len(data)):
             if n==0:
                 pass
             if data[n] not in Son:
-                Son[data[n]]=set()
+                Son[data[n]]=set(data[n-1])
             else:
                 Son[data[n]].add(data[n-1])
-        record=[data[0],
+    for specie in Name.items():
+        if specie[0] not in Son:
+            Son[specie[0]]=set()
+        if specie[0] not in Parent:
+            Parent[specie[0]]=list()
+        record=[specie[0],Name[specie[0]],Rank[specie[0]],Son[specie[0]],Parent[specie[0]]]
+        Taxon.append(record)
     return
 
 
 def Database():
     con=sqlite3.connect('./test/DB')
     cur=con.cursor()
-    cur.execute('create table if not exists taxon (Id text,Rank text,Name text,Son text,Parent text);')
-    for line in ToDB:
+    cur.execute('create table if not exists taxon (Id text,Name text,Rank text,Son text,Parent text);')
+    for line in Taxon:
         Son=' '.join(line[3])
         Parent=' '.join(line[4])
-        cur.execute('insert into taxon (Id,Rank,Name,Son,Parent) values (?,?,?,?,?);',(line[0],line[1],line[2],Son,Parent))
+        cur.execute('insert into taxon (Id,Name,Rank,Son,Parent) values (?,?,?,?,?);',(line[0],line[1],line[2],Son,Parent))
     con.commit()
     cur.close()
     con.close()
@@ -64,8 +71,7 @@ def Database():
 def Query():
     Querytype=input('1.by id\n2.by name\n')
     if Querytype not in ['1','2']:
-        print('wrong input!\n')
-        return 
+        raise ValueError('wrong input!\n')
     con=sqlite3.connect('./test/DB')
     cur=con.cursor()
     if Querytype=='1':
@@ -77,16 +83,15 @@ def Query():
         cur.execute('select * from taxon where Name like ?;',('%'+Name+'%',))
         Result=cur.fetchall()
 
-    for i in Result:
-        Id=i[0]
-        Rank=i[1]
-        Name=i[2]
-        Son=i[3].split(sep=' ')
-        Parent=i[4].split(sep=' ')
-
+    print(Result)
+    Id=Result[0]
+    Name=Result[1]
+    Rank=Result[2]
+    Son=Result[3].split(sep=' ')
+    Parent=Result[4].split(sep=' ')
     print('id    : ',Id)
-    print('rank  : ',Rank)
     print('name  : ',Name)
+    print('rank  : ',Rank)
     print('parent: ','->'.join(Parent))
     print('son   : ',','.join(Son))
     cur.close()
@@ -94,10 +99,10 @@ def Query():
     return 
 
 work=input('1.Init database\n2.query\n')
+if work not in ['1','2']:
+    raise ValueError('wrong input!\n')
 if work=='1':
     Create()
     Database()
 elif work=='2':
     Query()
-else:
-    print('wrong input!\n')
