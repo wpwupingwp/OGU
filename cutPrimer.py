8e65b77a244d2ab6e8ac64b448ff26a163938e4d
wuping <wpwupingwp@hotmail.com>
2014-08-09 17:20:12 +0800

    update lenght of cutprimer

--- cutPrimer.py
+++ cutPrimer.py
@@ -1,27 +1,30 @@
-with open('test') as In:
-		Raw=In.read()
+with open('in.fasta') as In:
+    Raw=In.read()
 Primer=list()
-Primer.append(['1','1',Raw[0:20]])
-point=20
-number=2
-fr='F'
+Primer.append([Raw[0:20],1,'F'])
+Primer.append([Raw[450:470],2,'F'])
+point=450
+number=1
+fr='R'
 for i in range(300):
-		if point>=len(Raw):
-				Primer.append([Raw[-1:-20],'end','end'])
-				break
-		point+=350
-		Primer.append([Raw[point:(point+20)],number,fr])
-		number+=1
-		fr='R'
-		point+=50
-		Primer.append([Raw[point:(point+20)],number,fr])
-		number+=1
-		fr='F'
+    if point>=len(Raw):
+        break
+    point+=30
+    p=Raw[point:(point+20)]
+    add=[p,number,fr]
+    Primer.append(add)
+    number+=2
+    fr='F'
+    point+=420
+    p=Raw[point:(point+20)]
+    add=[p,number,fr]
+    Primer.append(add)
+    number-=1
+    fr='R'
+
+handle=open('primer.fasta','a')
 for p in Primer:
-	print(''.join(['>',str(p[1]),p[2]]))
-	print(p[0])
-
-
-
-
-
+    handle.write(''.join(['>',str(p[1]),p[2]]))
+    handle.write('\n')
+    handle.write(p[0])
+    handle.write('\n')
