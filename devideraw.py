c524def1af40018db1e89e365092d8e528705fb7
wuping <wpwupingwp@hotmail.com>
2014-08-12 17:39:18 +0800

    fix _for in unknown

--- devideraw.py
+++ devideraw.py
@@ -6,6 +6,8 @@ import sys
 Primer=list()
 Out=list()
 Unknown=list()
+Sum=list()
+Sum.append(['all':0])
 with open(sys.argv[2],'r') as In:
     Raw=In.read().split(sep='\n')
 for line in Raw:
@@ -13,23 +15,34 @@ for line in Raw:
 Primer.pop(-1)
 Primer.pop(0)
 Sequence=SeqIO.parse(sys.argv[1],'fastq')
-n=0
-m=0
 for s in Sequence:
-    n+=1
-    head=str((s.seq)[0:20])
+    Sum[0][1]+=1
+    head=str((s.seq)[0:15])
     Unknown.append(s)
     for p in Primer:
-        for a in p2.align.localmx(head,p[0],1,-1,-0.5,-0.1):
-            score=a[2]
-            if score>=15:
+        if head in p[1] or head in p[2]:
                 add=[p[0],s]
                 Out.append(add)
                 Unknown.pop(Unknown.index(s))
-                m+=1
                 break
-print(n,m,len(Unknown))
-#for cp in Out:
-#    handle=open(cp[0],'a')
-#    SeqIO.write(cp[1],handle,'fastq')
-#SeqIO.write(Unknown,'unknown.fastq','fastq')
+for p in Unknown:
+    for p in Primer:
+        for a in p2.align.localms(head,p[0],1,-1,-0.5,-0.1):
+            #1,same -1,different -0.5,gap open -0.1,gap extend
+            score=a[2]
+        if score>=15:
+            add=[p[0],s]
+            Out.append(add)
+            Unknown.pop(Unknown.index(s))
+            break
+for cp in Out:
+    handle=open(cp[0],'a')
+    if cp[0] not in Sum:
+        Sum.append([cp[0],1)
+    else:
+        Sum[Sum.index(cp[0])][1]+=1
+    SeqIO.write(cp[1],handle,'fastq')
+SeqIO.write(Unknown,'unknown.fastq','fastq')
+with open('sum.csv','w') as Out:
+    for line in Sum:
+        Out.write(line)
