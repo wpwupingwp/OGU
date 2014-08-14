513767140a8d63afcde85ef516b799c1c8e7d76e
wuping <wpwupingwp@hotmail.com>
2014-08-14 10:55:54 +0800

    start merge blast.py and devideraw.py

--- devideraw.py
+++ devideraw.py
@@ -52,3 +52,46 @@ SeqIO.write(Toblast,'unknown.fasta','fasta')
 with open('sum.csv','w') as Out:
     for key,value in Sum.items():
         Out.write(' '.join([key,str(value),'\n']))
+#!/usr/bin/python3
+from Bio import SearchIO
+from Bio.Blast.Applications import NcbiblastnCommandline as nb
+from Bio.Blast import NCBIXML as nx 
+import sys
+
+def runblast():
+    cmd=nb(query='unknown.fasta',db='primer',task='blastn-short',evalue=0.001,outfmt=5,out='blast.result')
+    stdout,stderr=cmd()
+    return 
+
+def parse():
+    global BlastResult
+    BlastResult=dict()
+    In=open('blast.result','r')
+    Out=open('blast.log','a')
+    sys.stdout=Out
+    results=list(nx.parse(In))
+    for record in results:
+        for hit in record.alignments:
+            a=hit.hsps[0]
+            if a.score<15:
+                continue
+            BlastResult[record.query]=hit.hit_def
+            print(record.query,hit.hit_def,'\n',a)
+
+def newparse():
+    global BlastResult
+    BlastResult=dict()
+    Out=open('blast.log','a')
+#    sys.stdout=Out
+    results=list(SearchIO.parse('blast.result','blast-xml'))
+    for record in results:
+        if len(record)==0:
+            continue
+        else: 
+            tophit=record[0]
+        print(tophit.id,record.id,'\n',tophit)
+        BlastResult[record.id]=tophit.id
+
+runblast()
+newparse()
+
