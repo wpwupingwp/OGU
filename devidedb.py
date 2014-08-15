597fe49640808b1f59394df5506f1e22d00bd0a9
wuping <wpwupingwp@hotmail.com>
2014-08-15 11:27:19 +0800

    edit devidedb

--- devidedb.py
+++ devidedb.py
@@ -2,12 +2,12 @@
 from Bio import SeqIO
 import sys
 
-area=sys.argv[1][-1]
-with open(sys.argv[1],'r') as In:
+area=sys.argv[2][-1]
+with open(sys.argv[2],'r') as In:
     List=In.read().split(sep='\n')
 List.pop()
 handle=open('db'+area,'a')
-DB=SeqIO.parse('maindb','fasta')
+DB=SeqIO.parse(sys.argv[1],'fasta')
 for record in DB:
     name=record.id.split(sep='_')
     if name[0] in List:
