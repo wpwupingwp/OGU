d1c5bdc6cbf31688128f255991734e60543013ff
wuping <wpwupingwp@hotmail.com>
2014-08-15 15:43:59 +0800

    sort fna/qual

--- addname.py
+++ addname.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 import sys
 import re
-handle1=open(sys.argv[1],"r")
-handle2=open(sys.argv[2],"r")
-In=str(handle1.read())
-Out=str(handle2.read())
-handle1.close()
-handle2.close()
-Rawinfo=re.findall("(?<=\>)[0-9a-zA-Z_].*[0-9]{5}",In)
+from Bio import SeqIO
+
+with open(sys.argv[1],'r') as fna:
+    In=fna.read()
+with open(sys.argv[2],'r') as qual:
+    Out=qual.read()
+Rawinfo=re.findall('(?<=\>)[0-9a-zA-Z_].*[0-9]{5}',In)
 for name in Rawinfo:
     info=name[-15:]
     Out=re.sub(info,name,Out,count=1)
-handle3=open(sys.argv[2],"w")
-handle3.write(Out)
+with open(sys.argv[2],'w') as out:
+    out.write(Out)
+fna=list(SeqIO.parse(sys.argv[1],'fasta'))
+qual=list(SeqIO.parse(sys.argv[2],'qual'))
+fna.sort(key=lambda x:x.id)
+qual.sort(key=lambda x:x.id)
+SeqIO.write(fna,sys.argv[1],'fasta')
+SeqIO.write(qual,sys.argv[2],'qual')
