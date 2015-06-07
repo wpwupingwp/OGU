cbbca359e99e571749f7d44af4431fb3b6b3502d
wuping <wpwupingwp@hotmail.com>
2015-06-07 19:21:01 +0800

    edit  cutfastq

--- cutfastq.py
+++ cutfastq.py
@@ -1,10 +1,16 @@
 import sys
 from Bio import SeqIO
-
-maxmium=600000
-#after that, it's very slow (several days)
-
-raw=list(SeqIO.parse(sys.argv[1],'fastq'))
-trim=raw[:maxmium]
-SeqIO.write(trim,sys.argv[1].replace('.','_cut.'),'fastq')
-
+'''
+usage: python3 cutfastq.py fastqfile cut_number
+'''
+maxmium=int(int(sys.argv[2])/2)
+trim1=sys.argv[1].replace('.','-'+sys.argv[2]+'.')
+trim2=sys.argv[1].replace('.','-2-'+sys.argv[2]+'.')
+handle1=open(trim1,'a')
+handle2=open(trim2,'a')
+raw=SeqIO.parse(sys.argv[1],'fastq')
+for counts,reads in enumerate(raw):
+    if counts<maxmium:
+        SeqIO.write(reads,handle1,'fastq')
+    else:
+        SeqIO.write(reads,handle2,'fastq')
