32313025a727648056af1a4b0949d3a43dd6218e
Wu Ping <wpwupingwp@hotmail.com>
2015-06-14 11:58:54 +0000

    Use pandas.read_excel instead of xlrd

--- xlsMerge.py
+++ xlsMerge.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
 import glob
-import xlrd
+import pandas
 
 #To get all xls filename in present directory
 #For example, 'A23-4.xls'
 data = list()
 name_list = glob.glob('*.xls')
 for name in name_list:
-    book = xlrd.open_workbook(name)
 #Use first sheet
-    sheet = book.sheet_by_index(0)
+    sheet = pandas.read_excel(name)
     data.append([name,sheet])
-print(data)
+    print(sheet)
