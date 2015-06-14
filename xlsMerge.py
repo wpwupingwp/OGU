56aa63805065498c89f56a00be1b054375f5a341
Wu Ping <wpwupingwp@hotmail.com>
2015-06-14 20:31:10 +0000

    Use dict instead of list to store raw data

--- xlsMerge.py
+++ xlsMerge.py
@@ -5,29 +5,35 @@ import pandas
 
 def get_data(filenames, data):
     """Here it uses pandas.read_excel to get contents from all xls files in
-    present directory. Since pandas uses numpy.int64 to store data, it may be
-    a problem to manipulate huge numbers(larger than 2**63).  
+    present directory. Since pandas uses numpy.int64 to store data, it may
+    cause a problem to manipulate huge numbers(larger than 2**63).  
     """
     for name in filenames:
         sheet = pandas.read_excel(name)
-        data.append([name, sheet])
+        data[name] = sheet
 
 def analyse(raw, edited):
     """Library: A B
        Plate: A 56, B 26
-       Times: 6 7 9
-       !rm A28-10.xls
+       Times: 6 
+            There are A28 and others which have 7 or 10 tables. In order to
+            simplify the program, here it just omit tables from 7 to 10.
     """
-    for name, sheet in raw:
-        print(name[:5],type(sheet))
+    for name in raw.keys():
+        print(name[:5],type(raw[name]))
+        library = name[0]
+        plate = name[1:3]
+        times = name[4]
+
 
 def main():
-    """It uses glob.glob to get names of all xls files. Hence it should be run
-    in the directory which contains all xls files.
-    All xls filename should follow this principle:
-    A00-0.xls
+    """It uses glob.glob to get names of all xls files. Hence it should be 
+    run in the directory which contains all xls files.
+    All xls filename should follow this format:
+     A00-0.xls
+     01234
     """
-    raw_data = list()
+    raw_data = dict()
     analysed = list()
     name_list = glob.glob('*.xls')
     get_data(name_list, raw_data)
