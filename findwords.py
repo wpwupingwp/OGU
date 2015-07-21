361afe9e0317d34dbb74b379a775e23f5f8e9db2
wuping <wpwupingwp@hotmail.com>
2015-07-21 19:39:46 +0800

    add under

--- findwords.py
+++ findwords.py
@@ -3,20 +3,24 @@
 with open('/usr/share/dict/words', 'r') as In:
     wordlist = In.read().split(sep='\n')
 vowel = set('aeiouAEIOU')
+under = set('underUNDER')
 all_vowel = list()
+all_under = list()
 
-def is_all_vowel(word):
+
+def is_all_vowel(word, letters):
     for letter in word:
-        if letter not in vowel:
+        if letter not in letters:
             return False
     return True
 
 for word in wordlist:
-    if is_all_vowel(word) is True:
+    if is_all_vowel(word, vowel) is True:
         all_vowel.append(word)
+    if is_all_vowel(word, under) is True:
+        all_under.append(word)
 
-sorted(all_vowel, key = lambda x:len(x))
 
 print('word length')
-for word in all_vowel:
+for word in all_under:
     print(word,len(word))
