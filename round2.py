11891df4b86fdd5cacdec3debb2a380e320d890d
Wu Ping <wpwupingwp@hotmail.com>
2015-07-23 16:21:22 +0000

    starting analyse

--- round2.py
+++ round2.py
@@ -7,14 +7,11 @@ from scipy.stats import linregress
 
 
 def get_raw_data(filenames, data):
-    """Here it uses pandas.read_excel to get contents from all xls files in
-    present directory. Since pandas uses numpy.int64 to store data, it may
-    cause a problem to manipulate huge numbers(larger than 2**63).  
+    """It can accept xls format.
     """
     for name in filenames:
         sheet = pandas.read_excel(name)
-# drop '.xls' from name
-        data[name[:5]] = sheet
+        data[name] = sheet
 
 
 def initiate_sample_data(raw, sample):
@@ -27,53 +24,78 @@ def initiate_sample_data(raw, sample):
         Index([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 'Unnamed: 12'],
               dtype='object')
     sample name then will looks like:
-        A33-11D
+        P1-11D
     """
-    for library in ['A', 'B']:
-        for plate in range(56):
-            if library == 'B' and plate > 25:
-                continue
-            for idx in 'ABCDEFGH':
-                for col in ['02', '03', '04', '05', '06',
-                            '07', '08', '09', '10', '11']:
-                    name = ''.join([
-                        library,
-                        '{:{fill}2d}'.format(plate+1, fill='0'),
-                        '-',
-                        col,
-                        idx
-                    ])
-                    sample[name] = { 
-                        'raw': [0, 0, 0, 0, 0, 0],
-                        'ref_1': [0, 0, 0, 0, 0, 0],
-                        'ref_2': [0, 0, 0, 0, 0, 0]
-                    }
+    for plate in '1234567':
+        for idx in 'ABCDEFGH':
+            for col in ['01', '12']:
+                name = ''.join([
+                    'P',
+                    plate,
+                    '-',
+                    col,
+                    idx
+                ])
+                sample[name] = { 
+                    '50uM': [0, 0, 0, 0, 0, 0, 0],
+                    '20uM': [0, 0, 0, 0, 0, 0, 0],
+                    '10uM': [0, 0, 0, 0, 0, 0, 0],
+                    'ref1': [0, 0, 0, 0, 0, 0, 0],
+                    'ref2': [0, 0, 0, 0, 0, 0, 0],
+                    'ref3': [0, 0, 0, 0, 0, 0, 0],
+                    'ref4': [0, 0, 0, 0, 0, 0, 0],
+                    'ref5': [0, 0, 0, 0, 0, 0, 0],
+                    'ref6': [0, 0, 0, 0, 0, 0, 0]
+                }
 
 
 def get_sample_data(raw_data, sample):
-    """It is better to check the original data to ensure this function works
-    well.
+    """In round 2, data in left plate and right plate are in different order:
+            ref1 50uM 20uM 10uM ref2 ref3 50uM 20uM 10uM ref1 ref2 ref3
+    But in Plate 1 and 2, there are some data does not follow this rule.
     """
     for sheet_name, sheet in raw_data.items():
+        print(sheet_name, sheet)
         time = int(sheet_name[-1])
-        if time > 6:
-            continue
-        else:
-            time = time - 1
+        plate = int(sheet_name[1])
+        time = time - 1
         for idx in 'ABCDEFGH':
-            for col in ['02', '03', '04', '05', '06', '07', '08', '09', '10', '11']:
+            for col in ['01', '12']:
                 cell = ''.join([
                     sheet_name[0:-1],
                     col,
                     idx
                 ])
-                ref_1 = sheet[1][idx]
-                ref_2 = sheet[12][idx]
+                ref1 = sheet[1][idx]
+                ref2 = sheet[12][idx]
+                ref3 = None
+                ref4 = None
+                ref5 = None
+                ref6 = None
+                if plate >= 3:
+                    ref3 = sheet[6][idx]
+                    ref4 = sheet[11][idx]
+                if plate >= 5:
+                    ref5 = sheet[5][idx]
+                    ref6 = sheet[10][idx]
                 raw = sheet[int(col)][idx]
-                sample[cell]['raw'][time] = raw
-                sample[cell]['ref_1'][time] = ref_1
-                sample[cell]['ref_2'][time] = ref_2
-
+                if col == '01':
+                    fifty = sheet[2][idx]
+                    twenty = sheet[3][idx]
+                    ten = sheet[4][idx]
+                else:
+                    fifty = sheet[7][idx]
+                    twenty = sheet[8][idx]
+                    ten = sheet[9][idx]
+                sample[cell]['50uM'][time] = fifty
+                sample[cell]['20uM'][time] = twenty
+                sample[cell]['10uM'][time] = ten
+                sample[cell]['ref1'][time] = ref1
+                sample[cell]['ref2'][time] = ref2
+                sample[cell]['ref3'][time] = ref3
+                sample[cell]['ref4'][time] = ref4
+                sample[cell]['ref5'][time] = ref5
+                sample[cell]['ref6'][time] = ref6
 
 def analyse(sample_raw_data, analysis):
     """This function only use the first five points.
@@ -130,14 +152,7 @@ def main():
     """It uses glob.glob to get names of all xls files. Hence it should be 
     run in the directory which contains all xls files.
     All xls filename should follow this format:
-     A00-0.xls
-     01234
-    
-    Library: A B
-       Plate: A 56, B 26
-       Times: 6 
-            There are A28 and others which have 7 or 10 tables. In order to 
-            simplify the program, here it just omit tables from 7 to 10.
+     P0-0.xls
     """
     name_list = glob.glob('*-*')
     raw_data = dict()
@@ -149,19 +164,22 @@ def main():
         'cell',
         'slope_of_slope',
         'fold_1', 'fold_2', 'fold_3',
-        'slope_50uM', 'slope_20uM', 'slope_10uM', 
-        'slope_ref1', 'slope_ref2', 'slope_ref3',
-        'intercept_50uM', 'intercept_20uM', 'intercept_10uM',
-        'intercept_ref1', 'intercept_ref2', 'intercept_ref3',
-        'r^2_50uM', 'r^2_20uM', 'r^2_10uM',
-        'r^2_ref1', 'r^2_ref2', 'r^2_ref3',
+        'slope_50uM', 'slope_20uM', 'slope_10uM', 'slope_ref'
+        'intercept_50uM', 'intercept_20uM', 'intercept_10uM', 'intercept_ref',
+        'r^2_50uM', 'r^2_20uM', 'r^2_10uM', 'r^2_ref'
         'raw_1', 'raw_2', 'raw_3', 'raw_4', 'raw_5', 'raw_6', 'raw_7',
         'ref1_1', 'ref1_2', 'ref1_3', 'ref1_4', 'ref1_5', 'ref1_6', 'ref1_7', 
         'ref2_1', 'ref2_2', 'ref2_3', 'ref2_4', 'ref2_5', 'ref2_6', 'ref2_7', 
+        'ref3_1', 'ref3_2', 'ref3_3', 'ref3_4', 'ref3_5', 'ref3_6', 'ref3_7', 
+        'ref4_1', 'ref4_2', 'ref4_3', 'ref4_4', 'ref4_5', 'ref4_6', 'ref4_7', 
+        'ref5_1', 'ref5_2', 'ref5_3', 'ref5_4', 'ref5_5', 'ref5_6', 'ref5_7', 
+        'ref6_1', 'ref6_2', 'ref6_3', 'ref6_4', 'ref6_5', 'ref6_6', 'ref6_7', 
     ]]
     get_raw_data(name_list, raw_data)
     initiate_sample_data(raw_data, sample_raw_data)
     get_sample_data(raw_data, sample_raw_data)
+    for i in sample_raw_data:
+        print(i)
     analyse(sample_raw_data, analysis)
     output(analysis)
 
