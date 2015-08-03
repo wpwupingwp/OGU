bd4e28131d9367fc230970dd64c3ffb82b0137a9
Wu Ping <wpwupingwp@hotmail.com>
2015-08-03 16:00:07 +0000

    getCDS

--- getCDS.py
+++ getCDS.py
@@ -3,6 +3,18 @@ import sys
 from Bio import SeqIO
 
 def main():
+    wanted_gene = [
+        'accD', 'atpA', 'atpB', 'atpE', 'atpF', 'atpH', 'atpI', 'ccsA', 'cemA', 
+        'clpP', 'infA', 'matK', 'ndhA', 'ndhB', 'ndhC', 'ndhD', 'ndhE', 'ndhF', 
+        'ndhG', 'ndhH', 'ndhI', 'ndhJ', 'ndhK', 'petA', 'petB', 'petD', 'petG', 
+        'petL', 'petN', 'psaA', 'psaB', 'psaC', 'psaI', 'psaJ', 'psbA', 'psbB', 
+        'psbC', 'psbD', 'psbE', 'psbF', 'psbH', 'psbI', 'psbJ', 'psbK', 'psbL', 
+        'psbM', 'psbN', 'psbT', 'psbZ', 'rbcL', 'rpl14', 'rpl16', 'rpl2', 
+        'rpl20', 'rpl22', 'rpl23', 'rpl32', 'rpl33', 'rpl36', 'rpoA', 'rpoB', 
+        'rpoC1', 'rpoC2', 'rps11', 'rps12', 'rps14', 'rps15', 'rps16', 'rps18', 
+        'rps19', 'rps2', 'rps3', 'rps4', 'rps7', 'rps8', 'rrn16', 'rrn23', 
+        'rrn4.5', 'rrn5', 'ycf1', 'ycf2', 'ycf3', 'ycf4'
+    ]
     handle = open(sys.argv[1], 'r')
     data = SeqIO.parse(handle, 'gb')
     target = list()
@@ -12,9 +24,8 @@ def main():
         for feature in record.features:
             sequence = list()
             position = list()
-            if feature.type != 'gene' or 'gene' not in feature.qualifiers: 
+            if feature.type != 'CDS' or 'gene' not in feature.qualifiers: 
                 continue
-
             if feature.location_operator != 'join':
                 position.append([
                     int(feature.location.start), 
@@ -28,8 +39,10 @@ def main():
                         int(i.location.end)
                     ])
             for n, frag in enumerate(position):
-                sequence = str(record.seq[frag[0]:frag[1]])
                 name = str(feature.qualifiers['gene'][0]).replace(' ', '_')
+                if name not in wanted_gene:
+                    continue
+                sequence = str(record.seq[frag[0]:frag[1]])
                 if n > 0:
                     name = '-'.join([name, str(n+1)])
                 target.append([organism, accession, name, sequence])
