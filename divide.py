d73d32ec2ce5ae7edefaa8a069f15117463b804a
Wu Ping <wpwupingwp@hotmail.com>
2015-08-12 15:43:18 +0000

    start main in divide

--- divide.py
+++ divide.py
@@ -1,63 +1,107 @@
 #!/usr/bin/python3
+
+import sys
 from Bio import SeqIO
 from Bio import SearchIO
-from Bio.SeqRecord import SeqRecord
 from Bio.Blast.Applications import NcbiblastnCommandline as nb
-import sys
+from os import makedirs
+from os.path import exists
 
-def RunBlast():
-    cmd=nb(query='unknown.fasta',db='primer',task='blastn-short',evalue=0.001,outfmt=5,out=''.join([area,'-PB']))
-    stdout,stderr=cmd()
+def blast(option):
+    if option == '1':
+        query_file = './output/all.fasta'
+    else:
+        query_file = sys.argv[1]
+    cmd = nb(
+    #  num_threads=8,
+        query=query_file,
+        db=sys.argv[2], 
+        task='megablast', 
+        evalue=0.001, 
+        outfmt=5, 
+        # xml format
+        out='BlastResult.xml'
+    )
+    stdout, stderr = cmd()
     return 
 
-def Parse():
-    Out=open(''.join([area,'-PB.log']),'a')
-    sys.stdout=Out
-    results=list(SearchIO.parse(''.join([area,'-PB']),'blast-xml'))
-    for record in results:
-        if len(record)==0:
+def parse(target):
+    blast_result = list(SearchIO.parse('BlastResult.xml', 'blast-xml'))
+    for record in blast_result:
+        if len(record) == 0:
             continue
-        else: 
-            tophit=record[0]
-            #ignore multiple hsps
-        print(tophit.id,record.id,'\n',tophit)
-        BlastResult[record.id]=tophit.id
+        else:
+            tophit = record[0]
+        target.append([tophit[0][0].query, tophit[0][0].hit])
 
-#main
-area=sys.argv[1].replace('.fastq','')
-Out=list()
-Unknown=list()
-BlastResult=dict()
-Sum={'cp{:03d}'.format(n+1):0 for n in range(140)}
-Primer=list(SeqIO.parse(sys.argv[2],'fasta'))
-Sequences=SeqIO.parse(sys.argv[1],'fastq')
-all=0
-for record in Sequences:
-    all+=1
-    Unknown.append(record)
-    head=str((record.seq)[0:15])
-    for p in Primer:
-        if head in p.seq:
-            add=[p.id[:-1],record]
-            Out.append(add)
-            Unknown.pop(Unknown.index(record))
-            break
-SeqIO.write(Unknown,'unknown.fasta','fasta')
-RunBlast()
-Parse()
-for index,record in enumerate(Unknown):
-    if record.id in BlastResult:
-        add=[BlastResult[record.id][:-1],record]
-        Out.append(add)
-        Unknown.pop(index)
-for cp in Out:
-    handle=open(cp[0],'a')
-    Sum[cp[0]]+=1
-    SeqIO.write(cp[1],handle,'fastq')
-SeqIO.write(Unknown,'unknown.fastq','fastq')
-Sum['unknown']=len(Unknown)
-Sum['blasted']=len(BlastResult)
-Sum['all']=all
-with open(''.join([area,'-devideraw.csv']),'w') as Out:
-    for key,value in Sum.items():
-        Out.write(' '.join([key,str(value),'\n']))
+def output(target, option):
+    if option == '1':
+        for record in target:
+            gene = record[0].id.split(sep='|')[-1]
+            output_file = ''.join([
+                'output/',
+                sys.argv[2], 
+                '-',
+                gene, 
+                '.fasta'
+            ])
+            rename_seq = SeqRecord(
+                seq=record[1].seq, 
+                id='|'.join([
+                    gene,
+                    sys.argv[1],
+                    record[1].id
+                ]),
+                description=''
+            )
+            SeqIO.write(rename_seq, output_file, 'fasta')
+    else:
+        output_file = open('output/' + sys.argv[1] + '-filtered.fasta', 'w' )
+        contig_id = {i[0].id for i in target} 
+        query_file = SeqIO.parse(sys.argv[1], 'fasta')
+        for record in query_file:
+            if record.id in contig_id:
+                SeqIO.write(record, output_file, 'fasta')
+
+def main():
+    """\n
+    Usage:
+    python3 divide.py fastq_file barcode_file primer_file
+#    Before running this script, ensure you already put blast database file in
+    current path. Also, it assumes that you have installed BLAST suite before. 
+    To create the db file: 
+    makeblastdb -in infile -out outfile -dbtype nucl
+
+    Barcode file looks like this:
+    ATACG BOP00001
+    Primer file looks like this:
+    rbcL rbcLF ATCGATCGATCGA
+    From left to right, there are:
+    1. gene name
+    2. primer name
+    3. primer sequence"""
+
+    print(main.__doc__)
+    if not exists('output'):
+        makedirs('output')
+    #read barcode file
+    with open(sys.argv[2], 'r') as barcode_file:
+        barcode_raw = barcode_file.read().split(sep='\n')
+    barcode_raw.pop()
+    barcode = [i.split() for i in barcode_raw]
+    #read primer file
+    with open(sys.argv[3], 'r') as primer_file:
+        primer_raw = primer_file.read().split(sep='\n')
+    primer_raw.pop()
+    primer = [i.split() for i in primer_raw]
+    print(primer)
+    raise ValueError('test')
+
+    fasta_file = sys.argv[1].replace('fastq', 'fasta')
+    SeqIO.convert(sys.argv[1], 'fastq', fasta_file, 'fasta')
+    blast(option)
+    parse(target)
+    output(target, option)
+
+if __name__ =='__main__':
+    main()
