e10b9bcc4d7952940dc8d616b53251f0d79a12ff
Wu Ping <wpwupingwp@hotmail.com>
2015-08-12 13:07:57 +0000

    left.pop()

--- join_fq.py
+++ join_fq.py
@@ -7,10 +7,14 @@ with open(sys.argv[1], 'r') as l:
     left = l.read().split(sep='\n')
 with open(sys.argv[2], 'r') as r:
     right = r.read().split(sep='\n')
+print(left.pop())
+print(right.pop())
 seq = 'NNNNNNNNNN'
 qual = 'AAAAAAAAAA'
-for i, j in left, right:
-    print(i,j)
+#for i, j,index in enumerate(zip(left, right)):
+length = len(left)/4
+print(length)
+    
     
 
 
