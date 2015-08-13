47716461830c0c3fcdd1f036b569855645011344
Wu Ping <wpwupingwp@hotmail.com>
2015-08-13 17:26:24 +0000

    finish blast, test

--- divide.py
+++ divide.py
@@ -12,14 +12,16 @@ from os.path import exists
 from subprocess import call
 import sys
 
+
 def parse(target):
     blast_result = list(SearchIO.parse('BlastResult.xml', 'blast-xml'))
-    for record in blast_result:
+    for record in _result:
         if len(record) == 0:
             continue
         else:
             tophit = record[0]
         target.append([tophit[0][0].query, tophit[0][0].hit])
+
 
 def get_barcode_dict():
     with open(sys.argv[2], 'r') as input_file:
@@ -34,15 +36,14 @@ def get_barcode_dict():
 def step1():
     """
     Divide raw data via barcode.
-    ID were added into the beginning of sequence id.
-    Results were placed in 'output/'."""
+    ID were added into the beginning of sequence id."""
     print(step1.__doc__)
     barcode_dict = get_barcode_dict()
     fastq_raw = SeqIO.parse(sys.argv[1], 'fastq')
     total = 0
     not_found = 0
-    handle = open('output/step1.fastq', 'w')
-    handle2 = open('output/step1_miss.fastq', 'w')
+    handle = open('step1.fastq', 'w')
+    handle2 = open('step1_miss.fastq', 'w')
     for record in fastq_raw:
         total += 1
         record_barcode = str(record.seq[:5])
@@ -59,8 +60,8 @@ def step1():
             letter_annotations=record.letter_annotations
         )
         SeqIO.write(new_record, handle, 'fastq')
-    SeqIO.convert('output/step1.fastq', 'fastq', 
-                  'output/step1.fasta', 'fasta')
+    SeqIO.convert('step1.fastq', 'fastq', 
+                  'step1.fasta', 'fasta')
 
     return (not_found, total)
 
@@ -72,7 +73,7 @@ def blast(query_file, database):
         task='blastn-short', 
         evalue=1e-9, 
         outfmt=5, 
-        out='output/BlastResult.xml'
+        out='out/BlastResult.xml'
     )
     stdout, stderr = cmd()
 
@@ -86,46 +87,51 @@ def get_primer_list():
 
 
 def write_fasta(primer_list, skip):
-    handle = open('output/primer.fasta', 'w')
+    handle = open('primer.fasta', 'w')
     join_seq = 'NNNNNNNNNNNNNNN'
     for index in range(0, len(primer_list)-1, 2):
         left = primer_list[index][2][skip:]
         right = primer_list[index+1][2][skip:]
         short_primer = ''.join([left, join_seq, right])
         name = str(primer_list[index][0],)
-        handle.write(''.join(['>', name]))
-        handle.write(short_primer)
+        handle.write(''.join([
+            '>', 
+            name, 
+            '\n',
+            short_primer,
+            '\n'
+        ]))
     handle.close()
 
 
 def step2():
     """
-    BLAST fastq in first step against primer database.
-    Next, use BLAST result to divide again."""
+    BLAST fastq in first step against primer database.  Next, use BLAST 
+    result to divide again."""
     print(step2.__doc__)
     barcode_length = 5
     primer_adapter = 14
     skip = barcode_length + primer_adapter
     primer_list = get_primer_list()
     write_fasta(primer_list, skip)
-    call('makeblastdb -in output/primer.fasta -out output/primer -dbtype nucl') 
-    blast('output/step1.fasta', 'output/primer')
+    call('makeblastdb -in primer.fasta -out primer -dbtype nucl',
+         shell=True) 
+    blast('step1.fasta', 'primer')
 
 
 def main():
     """
     Usage:
     python3 divide.py fastqFile barcodeFile primerFile
-    Step 1, divide data by barcode.
-    Step 2, divide data by primer via BLAST.
+    Step 1, divide data by barcode. Step 2, divide data by primer via BLAST.
     Ensure that you have installed BLAST suite before. 
     Barcode file looks like this:
     ATACG,BOP00001
     Primer file looks like this:
     rbcL,rbcLF,ATCGATCGATCGA
     rbcL,rbcLR,TACGTACGTACG
-    To get these two files, save your excel file as csv file.
-    Be carefull of the order of  each pair of primers.
+    To get these two files, save your excel file as csv file.  Be carefull 
+    of the order of  each pair of primers.
     From left to right, there are:
     1. gene name
     2. primer name
@@ -133,8 +139,8 @@ def main():
     In this program, primer have common sequence whose length is 14, and
     barcode's is 5. Change them if necessary(in step2)."""
     print(main.__doc__)
-    if not exists('output'):
-        makedirs('output')
+    if not exists('out'):
+        makedirs('out')
     #miss_step1, total = step1()
     #print('''
     #Step1 results:
