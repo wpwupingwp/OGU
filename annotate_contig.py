14c564f65377d500d7ed81615a2bc72c07611d11
Wu Ping <wpwupingwp@hotmail.com>
2015-08-18 15:17:11 +0000

    fix bug via pycharm

--- annotate_contig.py
+++ annotate_contig.py
@@ -4,7 +4,7 @@ import sys
 from Bio import SeqIO
 from Bio import SearchIO
 from Bio.Blast.Applications import NcbiblastnCommandline as nb
-from Bio.SeqRecord import SeqRecord 
+from Bio.SeqRecord import SeqRecord
 from multiprocessing import cpu_count
 from os import makedirs
 from os.path import exists
@@ -12,7 +12,7 @@ from subprocess import call
 
 
 def get_gene():
-    #Edit it when necessary
+    # Edit it when necessary
     wanted_gene_list = [
         'accD', 'atpA', 'atpB', 'atpE', 'atpF', 'atpH', 'atpI', 'ccsA',
         'cemA', 'clpP', 'infA', 'matK', 'ndhA', 'ndhB', 'ndhC', 'ndhD',
@@ -30,38 +30,37 @@ def get_gene():
     genomes = SeqIO.parse(sys.argv[1], 'gb')
     for genome in genomes:
         for feature in genome.features:
-            if feature.type != 'gene' or 'gene' not in feature.qualifiers: 
+            if feature.type != 'gene' or 'gene' not in feature.qualifiers:
                 continue
-            sequence = list()
             position = list()
             if feature.location_operator != 'join':
                 position.append([
-                    int(feature.location.start), 
+                    int(feature.location.start),
                     int(feature.location.end)
                 ])
             else:
                 for i in feature.sub_features:
                     position.append([
-                        int(i.location.start), 
+                        int(i.location.start),
                         int(i.location.end)
                     ])
             for n, frag in enumerate(position):
                 name = str(feature.qualifiers['gene'][0]).replace(' ', '_')
-                if name not in wanted_gene:
+                if name not in wanted_gene_list:
                     continue
-                sequence = str(record.seq[frag[0]:frag[1]])
+                sequence = str(genome.seq[frag[0]:frag[1]])
                 if n > 0:
-                    name = '-'.join([name, str(n+1)])
+                    name = '-'.join([name, str(n + 1)])
                 fragment.append([name, sequence])
     return fragment
 
 
 def generate_query(fragment):
-    handle = open('cds.fasta' ,'w')
-    for gene in cds:
+    handle = open('cds.fasta', 'w')
+    for gene in fragment:
         handle.write(''.join([
-            '>',gene[0], '|', gene[1], '\n',
-            gene[2],'\n'
+            '>', gene[0], '|', gene[1], '\n',
+            gene[2], '\n'
         ]))
     handle.close()
     return 'fragment.fasta'
@@ -74,11 +73,11 @@ def blast(query_file, contig_file):
          shell=True)
     cmd = nb(
         num_threads=cpu_count(),
-        query='query_file',
+        query=query_file,
         db=contig_file,
-        task='blastn', 
-        evalue=0.001, 
-        outfmt=5, 
+        task='blastn',
+        evalue=0.001,
+        outfmt=5,
         out=xml_file
     )
     stdout, stderr = cmd()
@@ -94,8 +93,9 @@ def parse(xml_file):
         else:
             tophit = record[0]
         parse_result.append([tophit[0][0].hit.id, tophit[0][0].query.id])
-    #{contig.id:gene}
+    # {contig.id:gene}
     return dict(parse_result)
+
 
 def output(parse_result, contig_file, mode):
     contigs = SeqIO.parse(contig_file, 'fasta')
@@ -115,7 +115,9 @@ def output(parse_result, contig_file, mode):
             )
             SeqIO.write(new_seq, handle, 'fasta')
             if mode == '2':
-                minium_length = 200
+                minimum_length = 200
+                if len(contig.seq) <= minimum_length:
+                    continue
                 gene_file = '-'.join([annotated_contig, gene])
                 handle_gene = open(gene_file, 'a')
                 SeqIO.write(new_seq, handle_gene, 'fasta')
@@ -136,7 +138,7 @@ def main():
         annotated by gene name.
         2. Query contig in a whole genome. It only judge if contig was belong to
         genome of given genbank file. Also, contig less than 200bp will be
-        droped. You can edit 'minium_length' in output.
+        droped. You can edit 'minimum_length' in output.
     The final result is 'out/contig_file_annotated.fasta'.
     On default, it use Nicotiana.gb which was placed in current path."""
     print(main.__doc__)
@@ -147,7 +149,7 @@ def main():
         raise ValueError('Bad command!\n')
     contig_file = sys.argv[2]
     if mode == '1':
-        fragment = get_cds()
+        fragment = get_gene()
         query_file = generate_query(fragment)
     else:
         query_file = sys.argv[1].replace('.gb', '.fasta')
@@ -157,5 +159,5 @@ def main():
     output(parse_result, contig_file, mode)
 
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     main()
