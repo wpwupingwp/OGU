391ad34d90b4aa401b2560a602d94963919d5ab9
Wu Ping <wpwupingwp@hotmail.com>
2015-08-18 13:41:14 +0000

    use old wanted_gene

--- annotate_contig.py
+++ annotate_contig.py
@@ -14,20 +14,21 @@ from tempfile import TemporaryFile
 
 def get_cds():
     #Edit it when necessary
-    wanted_gene = [
-        'accD', 'atpA', 'atpB', 'atpE', 'atpF', 'atpH', 'atpI', 'ccsA', 'cemA', 
-        'clpP', 'infA', 'matK', 'ndhA', 'ndhB', 'ndhC', 'ndhD', 'ndhE', 'ndhF', 
-        'ndhG', 'ndhH', 'ndhI', 'ndhJ', 'ndhK', 'petA', 'petB', 'petD', 'petG', 
-        'petL', 'petN', 'psaA', 'psaB', 'psaC', 'psaI', 'psaJ', 'psbA', 'psbB', 
-        'psbC', 'psbD', 'psbE', 'psbF', 'psbH', 'psbI', 'psbJ', 'psbK', 'psbL', 
-        'psbM', 'psbN', 'psbT', 'psbZ', 'rbcL', 'rpl14', 'rpl16', 'rpl2', 
-        'rpl20', 'rpl22', 'rpl23', 'rpl32', 'rpl33', 'rpl36', 'rpoA', 'rpoB', 
-        'rpoC1', 'rpoC2', 'rps11', 'rps12', 'rps14', 'rps15', 'rps16', 'rps18', 
-        'rps19', 'rps2', 'rps3', 'rps4', 'rps7', 'rps8', 'rrn16', 'rrn23', 
-        'rrn4.5', 'rrn5', 'ycf1', 'ycf2', 'ycf3', 'ycf4'
+    wanted_gene_list = [
+        'accD', 'atpA', 'atpB', 'atpE', 'atpF', 'atpH', 'atpI', 'ccsA',
+        'cemA', 'clpP', 'infA', 'matK', 'ndhA', 'ndhB', 'ndhC', 'ndhD',
+        'ndhE', 'ndhF', 'ndhG', 'ndhH', 'ndhI', 'ndhJ', 'ndhK', 'petA',
+        'petB', 'petD', 'petG', 'petL', 'petN', 'psaA', 'psaB', 'psaC',
+        'psaI', 'psaJ', 'psbA', 'psbB', 'psbC', 'psbD', 'psbE', 'psbF',
+        'psbH', 'psbI', 'psbJ', 'psbK', 'psbL', 'psbM', 'psbN', 'psbT',
+        'psbZ', 'rbcL', 'rpl14', 'rpl16', 'rpl2', 'rpl20', 'rpl22',
+        'rpl23', 'rpl32', 'rpl33', 'rpl36', 'rpoA', 'rpoB', 'rpoC1',
+        'rpoC2', 'rps11', 'rps12', 'rps14', 'rps15', 'rps16', 'rps18',
+        'rps19', 'rps2', 'rps3', 'rps4', 'rps7', 'rps8', 'rrn16',
+        'rrn23', 'rrn4.5', 'rrn5', 'ycf1', 'ycf2', 'ycf3', 'ycf4'
     ]
-    genomes = SeqIO.parse(sys.argv[1], 'gb')
     cds = list()
+    genomes = SeqIO.parse(sys.argv[1], 'gb')
     for genome in genomes:
         organism = genome.annotations['organism'].replace(' ', '_')
         accession = genome.annotations['accessions'][0]
@@ -159,12 +160,12 @@ def main():
         raise ValueError('Bad command!\n')
     if mode == '1':
         cds = get_cds()
-
+######################
     out_cds(fragments, option)
     blast(option)
     parse(target)
     output(target, option)
-    call('makeblastdb -in primer.fasta -out primer -dbtype nucl',
+    call('makeblastdb -in primer.fasta -out primer -dbtype nucl')
 
 if __name__ =='__main__':
     main()
