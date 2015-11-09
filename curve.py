47596a67f72f56fef27f767195f60f0dfbdaf17e
Ping Wu <wpwupingwp@outlook.com>
2015-11-09 12:02:32 +0800

    add errorbar

--- curve.py
+++ curve.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 
 xdata = [-7,-6.3,-6,-5.7,-5.3,-5.1,-5,-4.7,-4.4]
 ydata = [1.04,0.99,0.93,0.82,0.33,0.20,0.13,0.03,0.02]
-t = arange(-8,-3,1e-3)
+yerror = [0.05,0.09,0.07,0.09,0.02,0.02,0.03,0.01,0.02]
+t = arange(-7.5,-4,1e-3)
 ic = 0.5
 #result from SPSS 21, R square equals 0.998
 spss_a = 1.023
@@ -17,7 +18,8 @@ def logistics(x, a, b, c):
 def fit(x):
     return logistics(x, spss_a, spss_b, spss_c) - ic
 
-plt.plot(t, logistics(t, spss_a, spss_b, spss_c), xdata, ydata, 'r^')
+plt.plot(t, logistics(t, spss_a, spss_b, spss_c))
+plt.errorbar(xdata, ydata, yerr=yerror, fmt='o', color='c', ecolor='r')
 plt.show()
 ic50 = 10**(6+fsolve(fit, -5))
 print('IC50 of this component is {0} μM'.format(ic50[0]))
