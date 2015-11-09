4707c96a3e751740a1de5c127bfaa26786f9bcc2
Ping Wu <wpwupingwp@outlook.com>
2015-11-09 16:36:26 +0800

    format output

--- curve.py
+++ curve.py
@@ -1,10 +1,14 @@
-from numpy import *
+﻿from numpy import *
 from scipy.optimize import *
 import matplotlib.pyplot as plt
+import sys
+
+with open('data.txt', 'r') as data:
 
 xdata = [-7,-6.3,-6,-5.7,-5.3,-5.1,-5,-4.7,-4.4]
 ydata = [1.04,0.99,0.93,0.82,0.33,0.20,0.13,0.03,0.02]
 yerror = [0.05,0.09,0.07,0.09,0.02,0.02,0.03,0.01,0.02]
+guess = (1, 1e10, -5)
 t = arange(-7.5,-4,1e-3)
 ic = 0.5
 #result from SPSS 21, R square equals 0.998
@@ -21,7 +25,11 @@ def fit(x):
 plt.plot(t, logistics(t, spss_a, spss_b, spss_c))
 plt.errorbar(xdata, ydata, yerr=yerror, fmt='o', color='c', ecolor='r')
 plt.show()
-ic50 = 10**(6+fsolve(fit, -5))
+ic50_log10 = fsolve(fit, -5)
+ic50 = 10**(6+ic50_log10)
+print(ic50_log10)
 print('IC50 of this component is {0} μM'.format(ic50[0]))
-a, b = curve_fit(logistics, xdata, ydata, p0=(1, 1e10, -5))
-print(a, b)
+popt, pcov = curve_fit(logistics, xdata, ydata, p0=guess)
+print('function: y={0}/(1+{1}*exp(-1*{2}*x))'.format(*popt))
+print('Standard deviation error: {0}'.format(sqrt(diag(pcov))))
+print(sqrt(pcov))
