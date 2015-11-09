e1310c9f084bc197d444e91a72d1a71f754d19e4
ping <ping@PingWu.ping>
2015-11-09 21:44:45 +0800

    seems ok

--- curve.py
+++ curve.py
@@ -3,6 +3,20 @@ from scipy.optimize import *
 import matplotlib.pyplot as plt
 import sys
 
+guess = (1, 1e10, -5)
+t = arange(-7.5,-4,1e-3)
+ic = 0.5
+#result from SPSS 21, R square equals 0.998
+a = 1.023
+b = 7.172e10
+c = -4.6
+
+def logistics(x, a, b, c):
+    return a/(1+b*exp(-1*c*x))
+
+def fit(x):
+    return logistics(x, a, b, c) - ic
+
 if len(sys.argv) >1:
     with open(sys.argv[1], 'r') as data:
         raw = data.read()
@@ -10,36 +24,22 @@ if len(sys.argv) >1:
         raw_1.pop(-1)
         raw_1.pop(0)
         raw_2 = [i.split(sep=',') for i in raw_1]
-        xdata = [i[0] for i in raw_2]
-        ydata = [i[1] for i in raw_2]
-        yerror = [i[2] for i in raw_2]
+        xdata = [float(i[0]) for i in raw_2]
+        ydata = [float(i[1]) for i in raw_2]
+        yerror = [float(i[2]) for i in raw_2]
 else:
     xdata = [-7,-6.3,-6,-5.7,-5.3,-5.1,-5,-4.7,-4.4]
     ydata = [1.04,0.99,0.93,0.82,0.33,0.20,0.13,0.03,0.02]
     yerror = [0.05,0.09,0.07,0.09,0.02,0.02,0.03,0.01,0.02]
 
-print(xdata,ydata,yerror)
-guess = (1, 1e10, -5)
-t = arange(-7.5,-4,1e-3)
-ic = 0.5
-#result from SPSS 21, R square equals 0.998
-spss_a = 1.023
-spss_b = 7.172e10
-spss_c = -4.6
-
-def logistics(x, a, b, c):
-    return a/(1+b*exp(-c*x))
-
-def fit(x):
-    return logistics(x, spss_a, spss_b, spss_c) - ic
-
-plt.plot(t, logistics(t, spss_a, spss_b, spss_c))
+popt, pcov = curve_fit(logistics, xdata, ydata, p0=guess)
+a, b, c = popt
+print('function: y={0}/(1+{1}*exp(-1*{2}*x))'.format(a, b, c))
+print('Standard deviation error: A {0:.3f} B {1:g} C {2:.3f}'.format(*sqrt(diag(pcov))))
+plt.plot(t, logistics(t, a, b, c))
 plt.errorbar(xdata, ydata, yerr=yerror, fmt='o', color='c', ecolor='r')
 plt.show()
 ic50_log10 = fsolve(fit, -5)
 ic50 = 10**(6+ic50_log10)
 print(ic50_log10)
 print('IC50 of this component is {0:.3f} μM'.format(ic50[0]))
-popt, pcov = curve_fit(logistics, xdata, ydata, p0=guess)
-print('function: y={0}/(1+{1}*exp(-1*{2}*x))'.format(*popt))
-print('Standard deviation error: \nA {0:.3f}\nB{1}\nC {2:.3f}'.format(*sqrt(diag(pcov))))
