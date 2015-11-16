5f76ed99901960bdfe5cf3dfb53bfb4aa3ad7833
Ping Wu <wpwupingwp@outlook.com>
2015-11-16 14:33:22 +0800

    fix join_fastq

--- join_fastq.py
+++ join_fastq.py
@@ -1,4 +1,4 @@
-from Bio import SeqIO
+﻿from Bio import SeqIO
 import sys
 
 left = SeqIO.parse(sys.argv[1], 'fastq')
@@ -14,7 +14,7 @@ for l,r in zip(left,right):
     r_qual = ''.join([chr(i+offset) for i in
               r.letter_annotations['phred_quality']])
     sequence = 'NNNNNNNNNN'.join([l_seq, r_seq])
-    quality = 'AAAAAAAAAA'.join([l_qual, r_qual])
+    quality = 'AAAAAAAAAA'.join([l_qual, r_qual[::-1]])
     name = l.description
     handle.write('@{0}\n{1}\n+\n{2}'.format(name,sequence,quality))
 
