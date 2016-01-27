41cec79b9693c8b37048e13fa5229def9e84ca96
ping <ping@PingWu.ping>
2016-01-27 14:00:07 +0800

    clean some useless files

--- filter.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-from Bio import SeqIO
-import sys
-
-raw = list(SeqIO.parse(sys.argv[1], 'fasta'))
-filtered = [i for i in raw if len(i.seq) > int(sys.argv[2])]
-SeqIO.write(filtered, sys.argv[1]+sys.argv[2], 'fasta')
