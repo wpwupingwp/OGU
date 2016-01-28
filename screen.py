54f17e33324c806fbde2d011a27e26b803713933
ping <ping@PingWu.ping>
2016-01-28 17:46:43 +0800

    add output

--- screen.py
+++ screen.py
@@ -9,10 +9,15 @@ parser.add_argument('-l', '--length', type=int, dest='length', default=0, help='
 parser.add_argument('-c', '--cover', type=float, dest='cover', default=0, help='minimal coverage')
 arg = parser.parse_args()
 
-raw = list(SeqIO.parse(arg.filename, 'fasta'))
+with open(arg.filename, 'r') as input_file:
+    raw = list(SeqIO.parse(input_file, 'fasta'))
+output_file = arg.filename + '.filtered'
+handle = open(output_file, 'w')
 for i in raw:
     m = re.search(r'length_(\d+)_cov_(\d+.\d)_', i.id)
     if m is not None:
-        length = m.group(1)
-        cover = m.group(2)
-        print(length,cover)
+        length = int(m.group(1))
+        cover = float(m.group(2))
+        if length > arg.length and cover > arg.cover:
+            SeqIO.write(i, handle, 'fasta')
+            print(length,cover)
