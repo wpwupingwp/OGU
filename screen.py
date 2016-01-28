3628d3e741657b57e4ae4178ea35a778ac01c1fa
ping <ping@PingWu.ping>
2016-01-28 17:05:03 +0800

    change default to zero

--- screen.py
+++ screen.py
@@ -5,7 +5,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('filename', help='fasta file')
-parser.add_argument('-l', '--length', type=int, dest='length', default=50, help='minimal sequence length')
-parser.add_argument('-c', '--cover', type=int, dest='cover', default=1, help='minimal coverage')
+parser.add_argument('-l', '--length', type=int, dest='length', default=0, help='minimal sequence length')
+parser.add_argument('-c', '--cover', type=int, dest='cover', default=0, help='minimal coverage')
 arg = parser.parse_args()
 print(arg.length, arg.cover)
