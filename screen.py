1697a585f570707a24094462a9d37ed820106908
Ping Wu <wpwupingwp@outlook.com>
2016-01-28 15:23:56 +0800

    add screen.py

---
+++ screen.py
@@ -0,0 +1,11 @@
+from Bio import SeqIO
+import re
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file', dest=filename, help='fasta file')
+parser.add_argument('-l', '--length', type=int, dest=length, default=200, help='minimal sequence length')
+parser.add_argument('-c', '--cover', type=int, dest=cover, default=1, help='minimal coverage')
+arg = parser.parse_args()
+print(arg)
