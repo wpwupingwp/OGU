dbc8b5b7b3a45edc39c6da460994818e758c214c
Ping Wu <wpwupingwp@outlook.com>
2016-01-28 16:37:02 +0800

    add parser

--- screen.py
+++ screen.py
@@ -4,8 +4,8 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('file', dest=filename, help='fasta file')
-parser.add_argument('-l', '--length', type=int, dest=length, default=200, help='minimal sequence length')
-parser.add_argument('-c', '--cover', type=int, dest=cover, default=1, help='minimal coverage')
+parser.add_argument('filename', help='fasta file')
+parser.add_argument('-l', '--length', type=int, dest='length', default=50, help='minimal sequence length')
+parser.add_argument('-c', '--cover', type=int, dest='cover', default=1, help='minimal coverage')
 arg = parser.parse_args()
-print(arg)
+print(arg.length, arg.cover)
