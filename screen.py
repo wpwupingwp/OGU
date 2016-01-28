075ef93150d295c686fb90859c5f75b78fdbfc9c
ping <ping@PingWu.ping>
2016-01-28 17:31:28 +0800

    add re.search

--- screen.py
+++ screen.py
@@ -6,6 +6,13 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('filename', help='fasta file')
 parser.add_argument('-l', '--length', type=int, dest='length', default=0, help='minimal sequence length')
-parser.add_argument('-c', '--cover', type=int, dest='cover', default=0, help='minimal coverage')
+parser.add_argument('-c', '--cover', type=float, dest='cover', default=0, help='minimal coverage')
 arg = parser.parse_args()
-print(arg.length, arg.cover)
+
+raw = list(SeqIO.parse(arg.filename, 'fasta'))
+for i in raw:
+    m = re.search(r'length_(\d+)_cov_(\d+.\d)_', i.id)
+    if m is not None:
+        length = m.group(1)
+        cover = m.group(2)
+        print(length,cover)
