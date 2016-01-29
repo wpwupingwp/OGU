9ec922d2565d6099b5cb692ae5a8bfe4b9241a93
Ping Wu (Debian) <wpwupingwp@hotmail.com>
2016-01-29 12:00:49 +0800

    get gene and sample name

--- bop.py
+++ bop.py
@@ -1,20 +1,21 @@
 #!/usr/local/bin/python3
 
-from glob import glob
 from Bio import SeqIO
+from os import listdir
 import argparse
 import re
 
 parser = argparse.ArgumentParser(description='''This program will add filename
 into fasta sequence's id, and merge same gene files into single one.''')
-parser.add_argument('path', default='./', help='target path, default is "./"')
+parser.add_argument('--path', default='./', help='target path, default is "./"')
 arg = parser.parse_args()
 
-filename = glob(arg.path+'*')
-print(filename)
-pattern = re.compile(r'.*/(.*)-(BOP\d{6}).fasta')
+filename = listdir(arg.path)
+pattern = re.compile(r'(.*)-(.*).fasta')
 for i in filename:
     match =pattern.search(i)
     if match is not None:
-        print(match.group(1), match.group(2))
+        gene = match.group(1)
+        sample = match.group(2)
+        print(gene, sample)
 
