31b97443a9424c27c27e3becda75fe22149d2345
Ping Wu (Debian) <wpwupingwp@hotmail.com>
2016-01-29 11:41:00 +0800

    add pattern into bop.py

--- bop.py
+++ bop.py
@@ -1,11 +1,20 @@
 #!/usr/local/bin/python3
 
-#This program is to add filename into fasta sequence's id, and merge same gene into one file.
-
 from glob import glob
 from Bio import SeqIO
 import argparse
 import re
 
-parser = argparse.ArgumentParser(description='This program will add filename
-into fasta sequence's id, and merge same gene files into single one.')
+parser = argparse.ArgumentParser(description='''This program will add filename
+into fasta sequence's id, and merge same gene files into single one.''')
+parser.add_argument('path', default='./', help='target path, default is "./"')
+arg = parser.parse_args()
+
+filename = glob(arg.path+'*')
+print(filename)
+pattern = re.compile(r'.*/(.*)-(BOP\d{6}).fasta')
+for i in filename:
+    match =pattern.search(i)
+    if match is not None:
+        print(match.group(1), match.group(2))
+
