893993448211210d466e3f022d5efdee12640f3f
Ping Wu <wpwupingwp@outlook.com>
2016-01-31 23:29:28 +0800

    waiting for test

--- cut_barcode.py
+++ cut_barcode.py
@@ -11,12 +11,25 @@ parser.add_argument('i', 'input', help='input fastq file')
 parser.add_argument('o', 'output', help='output fastq file')
 parser.add_argument('-t', '--barcode_type',default='5*2')
 arg = parser.parse_args()
-print(arg)
+#print(arg)
 if arg.t != '5*2':
     raise ValueError('Do not support this type of barcode.\n')
 
+check_length = len(arg.b-arg.c+arg.l)-1
 with open(arg.f, 'r') as barcode_file:
     barcode = re.findall('[ATCG]{0}(?>=,)'.format(arg.b), barcode_file.read())
-    print(barcode
-raw = SeqIO.parse(arg.i, 'fastq')
+barcode_dict = {i:None for i in barcode}
+barcode_link = dict()
+for i in barcode:
+    barcode_link[''.join([i[0:(arg.cut - 1 )], arg.link])] = None
 
+handle = open(arg.o, 'a') 
+raw = SeqIO.parse(arg.i, 'fastq')
+for sequence in raw:
+    if str(sequence.seq[:arg.b]) in barcode_dict:
+        pass
+    elif str(sequence.seq[arg.cut:check_length]) in barcode_link:
+        pass
+    else:
+        continue
+    SeqIO.write(sequence[arg.cut:], handle, 'fastq')
