1bfaca4f349f52b5cb07eaf93a1cab29460fc302
Ping Wu (Debian) <wpwupingwp@hotmail.com>
2016-02-01 15:19:54 +0800

    update parameters' name

--- cut_barcode.py
+++ cut_barcode.py
@@ -5,28 +5,28 @@ import re
 parser = argparse.ArgumentParser()
 parser.add_argument('-b', '--barcode_length', type=int, default=10, help='original barcode length')
 parser.add_argument('-c', '--cut', type=int, default=5, help='barcode cutted length')
-parser.add_argument('-l', '--linker', type=int, default=10, help='linker length')
-parser.add_argument('f', 'barcode_file')
-parser.add_argument('i', 'input', help='input fastq file')
-parser.add_argument('o', 'output', help='output fastq file')
+parser.add_argument('-l', '--linker', default='GTAGACTGCGTACC', help='linker length')
+parser.add_argument('-f', '--barcode_file', help='barcode_file')
+parser.add_argument('input', help='input fastq file')
+parser.add_argument('-o', '--output', help='output fastq file')
 parser.add_argument('-t', '--barcode_type',default='5*2')
 arg = parser.parse_args()
-#print(arg)
-if arg.t != '5*2':
+print(arg)
+if arg.barcode_type != '5*2':
     raise ValueError('Do not support this type of barcode.\n')
 
-check_length = len(arg.b-arg.c+arg.l)-1
-with open(arg.f, 'r') as barcode_file:
-    barcode = re.findall('[ATCG]{0}(?>=,)'.format(arg.b), barcode_file.read())
+check_length = arg.barcode_length - arg.cut + len(arg.linker) - 1
+with open(arg.barcode_file, 'r') as barcode_file:
+    barcode = re.findall('[ATCG]{0}(?>=,)'.format(arg.barcode_length), barcode_file.read())
 barcode_dict = {i:None for i in barcode}
 barcode_link = dict()
 for i in barcode:
-    barcode_link[''.join([i[0:(arg.cut - 1 )], arg.link])] = None
+    barcode_link[''.join([i[0:(arg.cut - 1 )], arg.linker])] = None
 
-handle = open(arg.o, 'a') 
-raw = SeqIO.parse(arg.i, 'fastq')
+handle = open(arg.output, 'a') 
+raw = SeqIO.parse(arg.input, 'fastq')
 for sequence in raw:
-    if str(sequence.seq[:arg.b]) in barcode_dict:
+    if str(sequence.seq[:arg.barcode_length]) in barcode_dict:
         pass
     elif str(sequence.seq[arg.cut:check_length]) in barcode_link:
         pass
