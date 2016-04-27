d3e8cd04a786a18140ed01436ae4836e0e99c609
Ping Wu (Debian) <wpwupingwp@hotmail.com>
2016-04-27 15:52:53 +0800

    add Template.py

---
+++ Template.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+
+from time import process_time
+import argparse
+
+parser = argparse.ArgumentParser(description='''This program will do
+something.''')
+parser.add_argument('--path', default='./', help='target path, default is "./"')
+arg = parser.parse_args()
+
+#start here
+
+#end
+print('Finished in {:.3f}s.'.format(process_time()))
