903a0f94d010bf05f9d5321d9be4b260242b061a
Ping Wu <wpwupingwp@outlook.com>
2016-06-26 21:59:59 +0800

    rewrite bop.py

--- bop.py
+++ bop.py
@@ -6,8 +6,8 @@ import argparse
 import re
 import os
 
-parser = argparse.ArgumentParser(description='''This program will add filename
-into fasta sequence's id, and merge same gene files into single one.''')
+parser = argparse.ArgumentParser(description='''After process of divide.py, this program will add filename 
+into fastq sequence's id, and merge same gene files into single one.''')
 parser.add_argument('--path', default='./', help='target path, default is "./"')
 arg = parser.parse_args()
 
@@ -26,12 +26,10 @@ for i in filename:
         gene = match.group(1)
         handle = open('merge-'+gene, 'a')
         sample = match.group(2)
-        fasta_file = SeqIO.parse(i, 'fasta')
+        fasta_file = SeqIO.parse(i, 'fastq')
         for sequence in fasta_file:
-            sequence.description = re.sub(gene, 
-                                          '-'.join([gene, sample]),
-                                          sequence.description)
+            sequence.description = '-'.join([gene, sample, sequence.description])
 #to avoid repeat replacement
             sequence.id = ''
-            SeqIO.write(sequence, handle, 'fasta')
+            SeqIO.write(sequence, handle, 'fastq')
 print('Finished in {:.3f}s.'.format(process_time()))
