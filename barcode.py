67e95acb280f1b61032caf869730019a4810f704
Ping Wu <wpwupingwp@outlook.com>
2016-09-02 23:48:47 +0800

    screen sequence using iteration

--- barcode.py
+++ barcode.py
@@ -3,6 +3,30 @@
 import argparse
 from Bio import SeqIO
 from glob import glob
+from time import process_time
+
+
+def screen_length(fasta_files, cut_off):
+    for fasta in fasta_files:
+        keep = 0
+        drop = 0
+        count = list()
+        handle = open(fasta, 'r')
+        handle_out = open(fasta.replace('.fasta', '.fas'), 'w')
+        f = SeqIO.parse(handle, 'fasta')
+        for record in f:
+            count.append(len(record))
+        average = sum(count) / len(count)
+        handle.seek(0)
+        f = SeqIO.parse(handle, 'fasta')
+        for record in f:
+            print(len(record))
+            if len(record) > average*cut_off:
+                keep += SeqIO.write(record, handle_out, 'fasta')
+            else:
+                drop += 1
+        print('{0} sequences longer than {1} were kept, dropped {2} short sequences.'.format(keep, average*cut_off, drop))
+        handle.close()
 
 
 def main():
@@ -11,8 +35,12 @@ def main():
     parser = argparse.ArgumentParser( description=main.__doc__)
     parser.add_argument('--path', default='.', 
                         help='target path, default is present directory')
-    parser.
+    parser.add_argument('--cut_off', default=0.8, type=int, help='cut off for screening length, those length shorter than average_length*cut_off will be drop off')
+    parser.print_help()
     arg = parser.parse_args() 
+    fasta_files = glob('*.fasta')
+    screen_length(fasta_files, arg.cut_off)
+    print('Cost {:.3f}s.\n'.format(process_time()))
 
 
 if __name__ == '__main__':
