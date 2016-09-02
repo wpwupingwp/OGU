dd575fca67d7caf1edabedf9a7fe16c1bc4886d6
Ping Wu <wpwupingwp@outlook.com>
2016-09-02 21:59:35 +0800

    update Template

--- Template.py
+++ Template.py
@@ -3,14 +3,18 @@
 from time import process_time
 import argparse
 
-parser = argparse.ArgumentParser(
-    description='''This program will do something.''')
-parser.add_argument('--path', default='./', 
-                    help='target path, default is "./"')
-parser.print_help()
-arg = parser.parse_args()
+def main():
+    """docstring
+    """
+    parser = argparse.ArgumentParser(description=main.__doc__)
+    parser.add_argument('--path', default='./', 
+                        help='target path, default is "./"')
+    parser.print_help()
+    arg = parser.parse_args()
+    #start here
+    
+    #end
+    print('Cost {:.3f}s.\n'.format(process_time()))
 
-#start here
-
-#end
-print('Cost {:.3f}s.\n'.format(process_time()))
+if __name__ == '__main__':
+    main()
