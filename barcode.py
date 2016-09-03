f8d6c56d46cbecd4fcecf29afef36a2aaffa7261
Ping Wu <wpwupingwp@outlook.com>
2016-09-03 11:12:33 +0800

    finish screen_merge()

--- barcode.py
+++ barcode.py
@@ -6,27 +6,32 @@ from glob import glob
 from time import process_time
 
 
-def screen_length(fasta_files, cut_off):
+def screen_merge(fasta_files, path, cut_off, sample):
+    merge = list()
+    output = list()
     for fasta in fasta_files:
-        keep = 0
-        drop = 0
         count = list()
         handle = open(fasta, 'r')
-        handle_out = open(fasta.replace('.fasta', '.fas'), 'w')
+        file_name = fasta.replace('.fasta', '.fas')
+        handle_out = open(file_name, 'w')
+        output.append(file_name)
         f = SeqIO.parse(handle, 'fasta')
         for record in f:
             count.append(len(record))
         average = sum(count) / len(count)
         handle.seek(0)
         f = SeqIO.parse(handle, 'fasta')
-        for record in f:
-            print(len(record))
+        for n, record in zip(range(sample), f):
             if len(record) > average*cut_off:
-                keep += SeqIO.write(record, handle_out, 'fasta')
-            else:
-                drop += 1
-        print('{0} sequences longer than {1} were kept, dropped {2} short sequences.'.format(keep, average*cut_off, drop))
+                merge.append(record)
+                SeqIO.write(record, handle_out, 'fasta')
         handle.close()
+        handle_out.close()
+    merge_file = path + '\merge.fas'
+    SeqIO.write(merge, merge_file, 'fasta')
+    merge.append(merge_file)
+    return merge
+
 
 
 def main():
@@ -35,11 +40,13 @@ def main():
     parser = argparse.ArgumentParser( description=main.__doc__)
     parser.add_argument('--path', default='.', 
                         help='target path, default is present directory')
-    parser.add_argument('--cut_off', default=0.8, type=int, help='cut off for screening length, those length shorter than average_length*cut_off will be drop off')
+    parser.add_argument('--cut_off', default=0.8, type=float, help='cut off for screening length, those length shorter than average_length*cut_off will be drop off')
+    parser.add_argument('--sample',default=5,type=int,help='sample selected from each group')
     parser.print_help()
     arg = parser.parse_args() 
-    fasta_files = glob('*.fasta')
-    screen_length(fasta_files, arg.cut_off)
+    fasta_files = glob(arg.path+'\*.fasta')
+    #merge = screen_merge(fasta_files, arg.path, arg.cut_off, arg.sample)
+    merge = screen_merge(fasta_files, **vars(arg))
     print('Cost {:.3f}s.\n'.format(process_time()))
 
 
