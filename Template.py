2d6eff7d4b27e5405c51ff5ee667a56c886f5753
Ping Wu <wpwupingwp@outlook.com>
2016-09-26 23:38:45 +0800

    update Template

--- Template.py
+++ Template.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 
-from time import process_time
 import argparse
+from timeit import default_timer as timer
+
 
 def main():
     """docstring
     """
+    start_time = timer()
     parser = argparse.ArgumentParser(description=main.__doc__)
-    parser.add_argument('--path', default='./', 
+    parser.add_argument('--path', default='./',
                         help='target path, default is "./"')
     parser.print_help()
     arg = parser.parse_args()
-    #start here
-    
-    #end
-    print('Cost {:.3f}s.\n'.format(process_time()))
+    # start here
+    print(arg)
+    # end
+    end_time = timer()
+    print('Cost {:.3f}s.\n'.format(end_time-start_time))
 
 if __name__ == '__main__':
     main()
