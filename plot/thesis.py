163b37533235bd944baf48cf5fcafd1405a306ac
Ping Wu <wpwupingwp@outlook.com>
2016-12-13 20:05:01 +0800

    use 'k--' for fit line

--- plot/thesis.py
+++ plot/thesis.py
@@ -63,7 +63,7 @@ def draw_fit(x, y):
     text = r'$f(x)={0:.3f}x+{1:.3f}, R^2={2:.3f}$'.format(
         slope, intercept, r_value**2)
     fit = [slope*i+intercept for i in x]
-    plt.plot(x[:arg.regression], fit, 'k-')
+    plt.plot(x[:arg.regression], fit, 'k--')
     plt.annotate(text, xy=(x[-1], y[-2]))
 
 
