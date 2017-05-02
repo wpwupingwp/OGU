4285c18d51bda77ded7c9479e2fed3d546e17f0e
Workstation <wpwupingwp@outlook.com>
2017-05-02 15:46:40 +0800

    use down.list

--- batch_query.py
+++ batch_query.py
@@ -4,6 +4,7 @@ from Bio import Entrez
 import argparse
 import os
 
+
 def get_list(list_file):
     down_list = list()
     with open(list_file, 'r') as raw:
@@ -11,10 +12,16 @@ def get_list(list_file):
             down_list.append(line.strip())
     return down_list
 
-def down(down_list):
+
+def down(taxon_name, min_len, max_len):
     EMAIL = 'wpwupingwp@outlook.com'
     Entrez.email = EMAIL
-    query = '{0}[Organism:exp] AND (plastid[filter] AND ("10000"[SLEN] : "50000"[SLEN]))'.format('txid4479')
+    FILTER = 'plastid'
+    # set 'noexp' to fetch only this level
+    EXP = 'exp'
+    query = '''{}[Organism:{}] AND ({}[filter] AND ("{}"[SLEN] :
+    "{}"[SLEN]))'''.format(taxon_name, EXP, FILTER, min_len, max_len)
+    print(query)
     handle = Entrez.read(Entrez.esearch(db='nuccore', term=query,
                                         usehistory='y',))
     genome_content = Entrez.efetch(db='nuccore',
@@ -28,8 +35,18 @@ def down(down_list):
 
 def main():
     arg = argparse.ArgumentParser()
-    arg.add_argument('list_file', help='Taxonomy name list, seperate by line')
+    arg.add_argument('-l', dest='list_file',
+                     help='Taxonomy name list, seperate by line')
     arg.add_argument('-o', dest='output', default='out', help='output path')
     arg.add_argument('-min_len', default=10, type=int, help='minium length')
     arg.add_argument('-max_len', default=50000, type=int,
                      help='maximum length')
+    arg = arg.parse_args()
+    down_list = get_list(arg.list_file)
+    print(1)
+    down(down_list[1], arg.min_len, arg.max_len)
+    print(2)
+
+
+if __name__ == '__main__':
+    main()
