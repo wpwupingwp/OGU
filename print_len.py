be33af9da4b8383f3863aff97d332e291242cff8
Workstation <wpwupingwp@outlook.com>
2017-05-22 10:20:12 +0800

    add print_len

---
+++ print_len.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from Bio import SeqIO
+from glob import glob
+
+
+for fasta in glob('*.fa*'):
+    print(fasta)
+    for record in SeqIO.parse(fasta, 'fasta'):
+        print(record.id, '\t', len(record.seq))
+    print()
