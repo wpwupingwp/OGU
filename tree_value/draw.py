1a4c42b523eec62115b2d2c8f9ec9d4d56f49bfd
Workstation <wpwupingwp@outlook.com>
2017-05-26 10:07:43 +0800

    use bio.phylo

--- tree_value/draw.py
+++ tree_value/draw.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
-a = p.read('./mega_bp.nwk', 'newick')
+from Bio import Phylo as p
+from sys import argv
+
+a = p.read(argv[1], 'newick')
 b = a.get_terminals()
 b_l = len(b)
 c = a.get_nonterminals()
 c_l = len(c)
+p.draw(a)
 print('{:.3f}'.format(c_l/b_l))
