09f3ac487ea17739f4e018a88cbeb67d71d6aa28
Workstation <wpwupingwp@outlook.com>
2017-06-05 16:30:37 +0800

    update organize

--- organize.py
+++ organize.py
@@ -4,4 +4,5 @@ from Bio import SeqIO
 from glob import glob
 
 for i in glob('*.fasta'):
-    SeqIO.convert(i, 'fasta', i+'.new', 'fasta')
+    n = SeqIO.convert(i, 'fasta', i+'.new', 'fasta')
+    print(n)
