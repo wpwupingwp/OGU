9d72d967c0b916159bf58749a31c4c3bd2f73ffc
Workstation <wpwupingwp@outlook.com>
2017-06-13 10:32:12 +0800

    fix bug

--- order_seqs.py
+++ order_seqs.py
@@ -11,18 +11,15 @@ for fasta in file_list:
     for record in SeqIO.parse(fasta, 'fasta'):
         seqs = re.search(pattern, record.id).group(1)
         seqs = int(seqs)
-        print(seqs)
         new.append([seqs, record])
     # descending
     new.sort(key=lambda x: x[0], reverse=True)
 
     for m, n in enumerate(new):
         n[0] = m + 1
-    for i in new:
-        print(i)
-    for n in range(1, len(new)):
-        with open('{}.fasta'.format(n), 'a') as merge, open(
-                '{}.{}'.format(fasta, n), 'a') as split:
+    for n in range(len(new)):
+        with open('{}.fasta'.format(n+1), 'a') as merge, open(
+                '{}.{}'.format(fasta, n+1), 'a') as split:
             SeqIO.write(new[n][1], merge, 'fasta')
             SeqIO.write(new[n][1], split, 'fasta')
 finish = input('Finish.')
