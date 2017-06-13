98c6e04d59d63fc2c37347f7957dd0bf7526d6a3
Workstation <wpwupingwp@outlook.com>
2017-06-13 17:33:31 +0800

    fix remove duplicate

--- remove_identical.py
+++ remove_identical.py
@@ -20,22 +20,29 @@ def main():
     file_format = test_format(argv[1])
     raw = SeqIO.parse(argv[1], file_format)
     hash_dict = dict()
-    duplicate = dict()
-    output = list()
+    before = 0
+    after = 0
     for record in raw:
+        before += 1
         h = hashlib.sha512()
         b_text = str(record.seq).encode('utf-8')
         h.update(b_text)
         hash_text = h.hexdigest()
         if hash_text in hash_dict:
-            if hash_text in duplicate:
-                duplicate[hash_text].append(record.id)
-            else:
-                duplicate[hash_text] = [record.id, ]
+            hash_dict[hash_text].append(record)
         else:
-            output.append(record)
-    with open(argv[1]+'.new', 'w') as output_file:
-        SeqIO.write(output, output_file, file_format)
+            hash_dict[hash_text] = [record, ]
+    for record in hash_dict.values():
+        if len(record) == 1:
+            after += 1
+            with open(argv[1]+'.new', 'a') as output:
+                SeqIO.write(record, output, file_format)
+        else:
+            id_list = [i.id for i in record]
+            print('Duplicated sequences:')
+            print('\t'.join(id_list))
+    print('Total {} sequences in {} format.'.format(before, file_format))
+    print('{} sequences left in the file {}.new.'.format(after, argv[1]))
 
 
 if __name__ == '__main__':
