3aebd3a4d9a2f4aaf0062773a663cdd49edd4ae5
Workstation <wpwupingwp@outlook.com>
2017-06-21 10:53:33 +0800

    start count_csv

--- count_csv.py
+++ count_csv.py
@@ -2,15 +2,23 @@
 
 from sys import argv
 
-raw = list()
+sample_info = dict()
 with open(argv[1], 'r') as input_file:
+    is_head = True
     for line in input_file:
         line = line.strip()
-        raw.append(line.split(','))
-head = {i[0]: list() for i in raw}
-print(head)
-for column in range(len(raw[0])):
-    if column == 0:
-        continue
-    for l
-
+        line = line.split(',')
+        if is_head:
+            head = line
+            is_head = False
+        else:
+            for column in range(len(head)):
+                item = line[column]
+                if item not in sample_info:
+                    sample_info[item] = [[i, 0] for i in head]
+                sample_info[item][column][1] = 1
+sample_info.pop('')
+print('Sample', '\t'.join(head))
+for i in sample_info.keys():
+    line = [j[1] for j in sample_info[i]]
+    print(i, line)
