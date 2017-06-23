56a9a52796caf667471cc38e28814ba15535dd85
Workstation <wpwupingwp@outlook.com>
2017-06-23 16:08:07 +0800

    now xml2fasta write log

--- xml2fasta.py
+++ xml2fasta.py
@@ -7,14 +7,26 @@ from sys import argv
 
 xml = SearchIO.parse(argv[1], 'blast-xml')
 for query in xml:
+    if query.description != '':
+        query.id = ''.join([query.id, query.description])
+    handle_log = open('{}.log'.format(query.id), 'w')
     if len(query) == 0:
-        print('{} not found!'.format(query.id))
+        handle_log.write('{} not found!\n'.format(query.id))
         continue
-    print(query.id)
-    handle = open('{}_{}.fasta'.format(query.id, query.description), 'w')
+    handle = open('{}.fasta'.format(query.id), 'w')
     SeqIO.write(query[0][0].query, handle, 'fasta')
     for hit in query:
         for hsp in hit:
-            print(hsp.bitscore, hsp.hit.id, hsp.hit.description)
+            species_name = hsp.hit.description.split(' ')
+            if species_name[0].isupper():
+                species_name = '_'.join(species_name[:3])
+            else:
+                species_name = '_'.join(species_name[:2])
+            info = '{}#{}#{}#{}_{}\n'.format(
+                query.id,
+                hsp.bitscore,
+                species_name,
+                hsp.hit.description, hsp.hit.id)
+            handle_log.write(info)
             hsp.hit.id = '{}|{}'.format(hsp.bitscore, hsp.hit.id)
             SeqIO.write(hsp.hit, handle, 'fasta')
