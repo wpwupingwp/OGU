279685d26256f77d03d3079210cd2f288eed157f
Workstation <wpwupingwp@outlook.com>
2017-06-23 15:33:35 +0800

    xml2fasta generate output for each query

--- xml2fasta.py
+++ xml2fasta.py
@@ -8,8 +8,13 @@ from sys import argv
 xml = SearchIO.parse(argv[1], 'blast-xml')
 for query in xml:
     if len(query) == 0:
-        print(query.id)
+        print('{} not found!'.format(query.id))
+        continue
+    print(query.id)
+    handle = open('{}_{}.fasta'.format(query.id, query.description), 'w')
+    SeqIO.write(query[0][0].query, handle, 'fasta')
     for hit in query:
-        print(len(hit))
         for hsp in hit:
-            print(hsp.bitscore)
+            print(hsp.bitscore, hsp.hit.id, hsp.hit.description)
+            hsp.hit.id = '{}|{}'.format(hsp.bitscore, hsp.hit.id)
+            SeqIO.write(hsp.hit, handle, 'fasta')
