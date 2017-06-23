053ae195632a3c29d328961541a9c62d9046574d
Workstation <wpwupingwp@outlook.com>
2017-06-23 13:52:17 +0800

    trim.py now have "-n" option for zhangwen

--- trim.py
+++ trim.py
@@ -1,13 +1,28 @@
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
-from sys import argv
+import argparse
 
-start, end = argv[2].split('-')
-start = int(start) - 1
-end = int(end)
-with open('{}.new.fasta'.format(argv[1]), 'w') as output:
-    for record in SeqIO.parse(argv[1], 'fasta'):
-        # new = record[0:start] + record[end:]
-        new = record[0:start] + SeqRecord('N'*(end-start), id=record.id,
-                                          description = record.description) + record[end:]
-        SeqIO.write(new, output, 'fasta')
+
+def main():
+    arg = argparse.ArgumentParser()
+    arg.add_argument('input', help='input fasta file')
+    arg.add_argument('location', help='fragment to cut, "x-y"')
+    arg.add_argument('-n', action='store_true',
+                     help='use N to replace where to cut')
+    arg = arg.parse_args()
+    start, end = arg.location.split('-')
+    start = int(start) - 1
+    end = int(end)
+    with open('{}.new.fasta'.format(arg.input), 'w') as output:
+        for record in SeqIO.parse(arg.input, 'fasta'):
+            if arg.n:
+                new = record[0:start] + SeqRecord(
+                    'N'*(end-start), id=record.id,
+                    description=record.description) + record[end:]
+            else:
+                new = record[0:start] + record[end:]
+            SeqIO.write(new, output, 'fasta')
+
+
+if __name__ == '__main__':
+    main()
