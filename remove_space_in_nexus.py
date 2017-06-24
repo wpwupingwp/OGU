8a6c144627b1f27831cf5719c32bbbd470013ebd
Workstation <wpwupingwp@outlook.com>
2017-06-24 14:05:08 +0800

    remove_space_in_nexus works fine

--- remove_space_in_nexus.py
+++ remove_space_in_nexus.py
@@ -4,11 +4,12 @@ from sys import argv
 import re
 
 
-pattern = re.compile(r'([A-Za-z]) ([A-Za-z])')
+pattern = re.compile(r"(^'?.+'?) +([ATCG-]{20})")
 with open(argv[1], 'r') as old, open(argv[1]+'.new', 'w') as new:
     for line in old:
-        if line.startswith("'"):
-            new_line = re.sub(pattern, r'\1_\2', line)
-            new.write(new_line)
-        else:
-            new.write(line)
+        a = re.search(pattern, line)
+        if a is not None:
+            old_id = a.group(1)
+            new_id = re.sub(r'\W', '_', old_id)
+            line = line.replace(old_id, new_id)
+        new.write(line)
