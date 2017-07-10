2498caf75c10c9a7c6a6d11557fef227fd62bb21
Workstation <wpwupingwp@outlook.com>
2017-07-10 10:34:22 +0800

    add convert

--- convert.py
+++ convert.py
@@ -4,4 +4,4 @@ from Bio import SeqIO
 from Bio.Alphabet import IUPAC
 from sys import argv
 
-SeqIO.convert(argv[1], 'fasta', argv[2], 'nexus', alphabet=IUPAC.ambiguous_dna)
+SeqIO.convert(*argv[1:], alphabet=IUPAC.ambiguous_dna)
