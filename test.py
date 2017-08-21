d5411a53690000c48a9e8dc222329f8125e2750d
Workstation <wpwupingwp@outlook.com>
2017-08-21 17:54:20 +0800

    update test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 
 from functools import wraps
 from timeit import default_timer as timer
-from Bio import AlignIO
+from Bio import AlignIO, SeqIO
 import numpy as np
 
 
@@ -55,17 +55,46 @@ def read():
 
 
 @print_time
+def read2():
+    a = list(SeqIO.parse('./aln', 'fasta'))
+    return a
+
+
+@print_time
+def read3():
+    a = list()
+    i = None
+    with open('./aln', 'r') as raw:
+        for line in raw:
+            if line.startswith('>'):
+                a.append(i)
+                i = list()
+            else:
+                i.append(line.strip())
+    a = a[1:]
+    return a
+
+
+@print_time
 def main():
     """The result shows that convert+numpy is abouth twice faster than
     Bio.AlignIO iterate. And the order 'F' is faster than 'C' order(~1.4
-    times).
+    times)
+    If not use Biopython's fasta reader, convert is 70x faster and numpy is
+    17x faster than old. At that time, order 'F' and order 'C' have slight
+    difference.
     """
     # start here
-    a = read()
-    test_bio(a)
+    # a = read()
+    # a = convert(a)
+    # test_bio(a)
+    a = read2()
     a = convert(a)
     test_np(a)
-    a = read()
+    a = read3()
+    a = convert(a, order='F')
+    test_np(a)
+    a = read3()
     a = convert(a, order='F')
     test_np(a)
     # end
