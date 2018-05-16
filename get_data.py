51e44c1325410c234633ba85c64172cd7da70101
Workstation <wpwupingwp@outlook.com>
2018-05-16 17:35:15 +0800

    start get_data

---
+++ get_data.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import argparse
+import os
+from functools import wraps
+from timeit import default_timer as timer
+
+
+def print_time(function):
+    @wraps(function)
+    def wrapper(*args, **kargs):
+        start = timer()
+        result = function(*args, **kargs)
+        end = timer()
+        print('Cost {1:3f}s.\n'.format(end-start))
+        return result
+    return wrapper
+
+
+@print_time
+def function():
+    pass
+
+
+def parse_args():
+    arg = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description=main.__doc__)
+    arg.add_argument('-taxon', required=True, help='Taxonomy name')
+    arg.add_argument('-min_len', default=100, type=int, help='minium length')
+    arg.add_argument('-max_len', default=10000, type=int,
+                     help='maximum length')
+    arg.add_argument('-molecular', choices=('DNA', 'RNA'), default='DNA',
+                     help='molecular type')
+    arg.add_argument('-organelle', default='plastid',
+                     choices=('mitochondrion', 'plastid', 'chloroplast'),
+                     help='organelle type')
+    arg.add_argument('-group', default='plants',
+                     choices=('animals', 'plants', 'fungi', 'protists',
+                              'bacteria', 'archaea', 'viruses'),
+                     help='Species kind')
+    arg.add_argument('-out', default='out', help='output directory')
+    arg.print_help()
+    return arg.parse_args()
+
+
+#biomol_genomic[PROP]
+#biomol_mrna[PROP]
+#[filter]
+def main():
+    """Get data from Genbank.
+    """
+    arg = parse_args()
+    # start here
+    if not os.path.exists(arg.out):
+        os.mkdir(arg.out)
+    function()
+    # end
+
+
+if __name__ == '__main__':
+    main()
