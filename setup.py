0890e79276c56da4e69d716b182e5dd1f15ed524
Workstation <wpwupingwp@outlook.com>
2018-12-05 23:05:42 +0800

    0.9.13

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     py_modules=['BarcodeFinder'],
     url='https://github.com/wpwupingwp/BarcodeFinder',
-    version='0.9.12',
+    version='0.9.13',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
