5502be32d185ef2c5973369b534e12d21488369d
Workstation <wpwupingwp@outlook.com>
2019-02-20 18:57:34 +0800

    remove useless unpack

--- BarcodeFinder.py
+++ BarcodeFinder.py
@@ -1431,29 +1431,37 @@ def count_and_draw(alignment, arg):
     min_primer = arg.min_primer
     max_product = arg.max_product
     step = arg.step
-    # r_list, h_list, pi_list, t_list : count, normalized entropy, Pi and
-    # tree value
-    r_list = []
-    h_list = []
+    # gap_list, r_list, h_list, pi_list, t_list : count, normalized entropy,
+    # Pi and tree value
+    gap_ratio_list = []
+    entropy_list = []
+    avg_branch_len_list = []
     pi_list = []
-    t_list = []
-    l_list = []
+    observed_res_list = []
+    tree_res_list = []
     max_h = np.log2(rows)
     index = []
     max_plus = max_product - min_primer * 2
     max_range = columns - max_product
+    handle = open(output + '.variance.tsv', 'w', encoding='utf-8')
+    # iqtree blmin is 1e-6
+    fmt = '{},{:.2%},{:.6f},{:.6f},{:.6f},{:.6f},{:.6f}\n'
+    handle.write('Index,GapRatio,Resolution,TreeValue,AvgTerminalBranchLen,'
+                 'Entropy,Pi\n')
     for i in range(0, max_range, step):
         # skip gap
         # if consensus.sequence[i] in ('-', 'N'):
         #     continue
         # exclude primer sequence
-        resolution, entropy, pi, tree_value, avg_t_branch_len = get_resolution(
-            alignment, i, i + max_plus, arg.fast)
-        r_list.append(resolution)
-        h_list.append(entropy / max_h)
+        values = get_resolution(alignment, i, i + max_plus, arg.fast)
+        handle.write(fmt.format(*values))
+        gap_ratio, resolution, entropy, pi, tree_value, avg_branch_len = values
+        gap_ratio_list.append(gap_ratio)
+        observed_res_list.append(resolution)
+        entropy_list.append(entropy / max_h)
         pi_list.append(pi)
-        t_list.append(tree_value)
-        l_list.append(avg_t_branch_len)
+        tree_res_list.append(tree_value)
+        avg_branch_len_list.append(avg_branch_len)
         index.append(i)
 
     plt.style.use('seaborn-colorblind')
@@ -1469,13 +1477,14 @@ def count_and_draw(alignment, arg):
     ax2.set_ylabel(r'$\pi$', rotation=-90, labelpad=20)
     # plt.xticks(np.linspace(0, max_range, 21))
     if not arg.fast:
-        ax1.plot(index, t_list, label='Tree Resolution', alpha=0.8)
-        ax2.plot(index, l_list, linestyle='--',
+        ax1.plot(index, tree_res_list, label='Tree Resolution', alpha=0.8)
+        ax2.plot(index, avg_branch_len_list, linestyle='--',
                  label='Average Terminal Branch Length', alpha=0.8)
         ax2.set_ylabel(r'$\pi$ and Average Branch Length', rotation=-90,
                        labelpad=20)
-    ax1.plot(index, h_list, label='Shannon Equitability Index', alpha=0.8)
-    ax1.plot(index, r_list, label='Observed Resolution', alpha=0.8)
+    ax1.plot(index, entropy_list, label='Shannon Equitability Index',
+             alpha=0.8)
+    ax1.plot(index, observed_res_list, label='Observed Resolution', alpha=0.8)
     ax1.legend(loc='lower left')
     ax2.plot(index, pi_list, 'k--', label=r'$\pi$', alpha=0.8)
     # _ = round(np.log10(max(Pi))) + 1
@@ -1486,14 +1495,9 @@ def count_and_draw(alignment, arg):
     plt.savefig(output + '.png')
     plt.close()
     # plt.show()
-    with open(output + '.variance.tsv', 'w', encoding='utf-8') as _:
-        _.write('Index,Resolution,TreeValue,AvgTerminalBranchLen,Entropy,Pi\n')
-        for i, r, t, l, h, pi in zip(index, r_list, t_list, l_list, h_list,
-                                     pi_list):
-            # iqtree blmin is 1e-6
-            _.write('{},{:.6f},{:.6f},{:.6f},{:.6f},{:.6f}\n'.format(i, r, t,
-                                                                     l, h, pi))
-    return r_list, h_list, pi_list, t_list, l_list, index
+    handle.close()
+    return (gap_ratio_list, observed_res_list, entropy_list, pi_list,
+            tree_res_list, avg_branch_len_list, index)
 
 
 def parse_blast_tab(filename):
