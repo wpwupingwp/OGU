d24a9a6515f69ae8a0287ed0de954bba8d0ba41c
Ping Wu <wpwupingwp@outlook.com>
2020-12-24 21:23:12 +0800

    remove old check_tools

--- BarcodeFinder/utils.py
+++ BarcodeFinder/utils.py
@@ -302,10 +302,10 @@ def get_blast(third_party=None) -> (bool, str):
     urls = {'Linux': url+'-x64-linux.tar.gz',
             'Darwin': url+'-x64-macosx.tar.gz',
             'Windows': url+'-x64-win64.tar.gz'}
-    if test_cmd(blast)
+    if test_cmd(blast):
         ok = True
         return ok, blast
-    if test_cmd(home_blast)
+    if test_cmd(home_blast):
         ok = True
         return ok, str(home_blast)
     log.warning('Cannot find NCBI BLAST, try to install.')
@@ -367,10 +367,10 @@ def get_iqtree(third_party=None) -> (bool, str):
     system = platform.system()
     filename = fileinfo[system][0]
     home_iqtree = third_party / fileinfo[system][1] / 'bin' / iqtree
-    if test_cmd(iqtree)
+    if test_cmd(iqtree):
         ok = True
         return ok, iqtree
-    if test_cmd(home_iqtree)
+    if test_cmd(home_iqtree):
         ok = True
         return ok, str(home_iqtree)
     log.warning('Cannot find iqtree, try to install.')
@@ -428,10 +428,10 @@ def get_mafft(third_party=None) -> (bool, str):
                 'Windows': ('mafft-7.475-win64-signed.zip', 'mafft-win')}
     system = platform.system()
     home_mafft = third_party / fileinfo[system][1] / mafft
-    if test_cmd(mafft)
+    if test_cmd(mafft):
         ok = True
         return ok, mafft
-    if test_cmd(home_mafft)
+    if test_cmd(home_mafft):
         ok = True
         return ok, str(home_mafft)
     log.warning('Cannot find iqtree, try to install.')
@@ -445,7 +445,10 @@ def get_mafft(third_party=None) -> (bool, str):
         return ok, ''
     try:
         # file is ~10mb
-        down = urlopen(url+fileinfo[system][0], timeout=1000)
+        if system != 'Darwin':
+            down = urlopen(url+fileinfo[system][0], timeout=1000)
+        else:
+            down = urlopen(url+fileinfo[system][0]+'?signed', timeout=1000)
     except Exception:
         log.critical('Cannot download mafft.')
         log.critical(f'Please manually download it from {url}')
@@ -486,89 +489,6 @@ def get_all_third_party():
     return
 
 
-def deploy(software):
-    """
-    According to system, install software.
-    Return False if failed
-    """
-    log.info('Try to install {}.'.format(software))
-    log.warning('Please consider to install it following official '
-                'instruction to get a CLEAN system.')
-    sys = system()
-    # url dict
-    blast_url = ('ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.8.1/'
-                 'ncbi-blast-2.8.1+')
-    iqtree_url = ('https://github.com/Cibiv/IQ-TREE/releases/download/v1.6.9/'
-                  'iqtree-1.6.9')
-    mafft_url = 'https://mafft.cbrc.jp/alignment/software/mafft'
-    # windows blast path not sure
-    urls = {'Linux':
-                {'BLAST': {'url': blast_url+'-x64-linux.tar.gz',
-                           'path': abspath('ncbi-blast-2.8.1+'+sep+'bin')},
-                 'IQTREE': {'url': iqtree_url+'-Linux.tar.gz',
-                            'path': abspath('iqtree-1.6.9-Linux'+sep+'bin')},
-                 'MAFFT': {'url': mafft_url+'-7.407-linux.tgz',
-                           'path': abspath('mafft-linux64')}},
-            'Darwin':
-                {'BLAST': {'url': blast_url+'-x64-macosx.tar.gz',
-                           'path': abspath('ncbi-blast-2.8.1+'+sep+'bin')},
-                 'IQTREE': {'url': iqtree_url+'-MacOSX.zip',
-                            'path': abspath('iqtree-1.6.9-MacOSX'+sep+'bin')},
-                 'MAFFT': {'url': mafft_url+'-7.407-mac.zip',
-                           'path': abspath('mafft-mac')}},
-            'Windows':
-                {'BLAST': {'url': blast_url+'-win64.exe',
-                           'path': abspath('cli')},
-                 'IQTREE': {'url': iqtree_url+'-Windows.zip',
-                            'path': abspath('iqtree-1.6.9-Windows'+sep+'bin')},
-                 'MAFFT': {'url': mafft_url+'-7.409-win64-signed.zip',
-                           'path': abspath('mafft-win')}}}
-    url = urls[sys][software]['url']
-    # down
-    if sys == 'Windows':
-        if not download_software(url):
-            return None
-        if software == 'BLAST':
-            log.info('BLAST on Windows has to been installed manually.')
-            log.info('Please configure PATH variable correctly.')
-            run('ncbi-blast-2.8.1+-win64.exe', shell=True)
-    elif sys == 'Linux':
-        ok = False
-        for pack_mgr in ('apt', 'dnf', 'yum', 'pkg'):
-            r = run('sudo {} install ncbi-blast+ iqtree mafft'.format(
-                pack_mgr), shell=True)
-            if r.returncode == 0:
-                ok = True
-                break
-        if not ok:
-            log.info('Cannot install {} with package manager. Try to '
-                     'download.'.format(software))
-            if not download_software(url):
-                return None
-    elif sys == 'Darwin':
-        with open(devnull, 'w', encoding='utf-8') as f:
-            r = run('brew --help', shell=True, stdout=f, stderr=f)
-        if r.returncode == 0:
-            run('brew install blast mafft brewsci/science/iqtree', shell=True)
-        else:
-            log.warning('Cannot find Homebrew.')
-            if not download_software(url):
-                return None
-            # after unzip, file lost executable flag on mac system
-            run('chmod +x {}'.format(join_path(urls[sys][software]['path'],
-                                               '*')), shell=True)
-            if software == 'MAFFT':
-                run('chmod +x {}'.format(join_path(urls[sys]['MAFFT']['path'],
-                                                   'mafftdir', 'bin', '*')),
-                    shell=True)
-                run('chmod +x {}'.format(join_path(urls[sys]['MAFFT']['path'],
-                                                   'mafftdir', 'libexec',
-                                                   '*')), shell=True)
-    # windows can omit .bat, linux cannot
-    if software == 'MAFFT' and sys != 'Windows':
-        rename(join_path(urls[sys]['MAFFT']['path'], 'mafft.bat'),
-               join_path(urls[sys]['MAFFT']['path'], 'mafft'))
-    return abspath(urls[sys][software]['path'])
 
 
 def parse_blast_tab(filename):
