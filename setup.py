65783becc832e94879640e003d3c2e0639663367
Ping Wu <wpwupingwp@outlook.com>
2021-02-19 11:53:22 +0800

    update tag

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ setuptools.setup(
     name='BarcodeFinder',
     packages=setuptools.find_packages(),
     url='https://github.com/wpwupingwp/BarcodeFinder',
-    version='0.9.47',
+    version='0.9.48',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
