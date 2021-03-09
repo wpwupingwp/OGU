40bfd16e1652a1297491557dfc1b339bfc1c7997
Ping Wu <wpwupingwp@outlook.com>
2021-03-09 10:13:42 +0800

    fix y label

--- BarcodeFinder/evaluate.py
+++ BarcodeFinder/evaluate.py
@@ -484,7 +484,7 @@ def output_sliding(sliding: list, name: str, out: Path,
     # different ytick
     ax2 = ax1.twinx()
     # ax2..yaxis.set_ticks(np.linspace(0, max_range, 21))
-    ax2.set_ylabel(r'$\pi$ & Phylogenetic Diversity', rotation=-90, labelpad=20)
+    ax2.set_ylabel(r'Phylogenetic Diversity', rotation=-90, labelpad=20)
     ax2.plot(index, [i.PD for i in sliding], linestyle='--', label='PD',
              alpha=0.8)
     ax2.plot(index, [i.PD_stem for i in sliding], linestyle='--',
