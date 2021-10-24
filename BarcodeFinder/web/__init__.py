e036c6aeecf8d2f96fa086785d8e0fdb56c9ed25
Ping Wu <wpwupingwp@outlook.com>
2021-10-24 17:11:03 +0800

    start web

---
+++ BarcodeFinder/web/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+app.config.from_pyfile('config.py')
+
+from web import config
+from web import views
+from web.database import db
