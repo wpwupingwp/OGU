92b294dccc4a867ecc6c852ccbb3c53df3b3919d
Ping Wu <wuping@sicnu.edu.cn>
2022-04-12 16:00:42 +0800

    try to fix mafft path

--- BarcodeFinder/utils.py
+++ BarcodeFinder/utils.py
@@ -526,7 +526,7 @@ def get_mafft(third_party=None, result=None) -> (bool, str):
     ok = False
     original_url = 'https://mafft.cbrc.jp/alignment/software/'
     # system: {filename, folder}
-    fileinfo = {'Linux': ('mafft-7.490-linux.tgz', 'mafft-linux64'),
+    fileinfo = {'Linux': ('mafft-7.490-linux.tgz', 'mafft-linux64/mafftdir/bin'),
                 'Darwin': ('mafft-7.490-mac.zip', 'mafft-mac/mafftdir/bin'),
                 'Windows': ('mafft-7.490-win64-signed.zip', 'mafft-win')}
     home_mafft = third_party / fileinfo[system][1] / mafft
@@ -545,8 +545,10 @@ def get_mafft(third_party=None, result=None) -> (bool, str):
         ok = get_software(mafft, down_url, down_file, third_party, home_mafft,
                           test_option=test_option)
         libexec = home_mafft.parent.parent / 'libexec'
-        for file in libexec.iterdir():
-            file.chmod(0o755)
+        # windows use different path
+        if libexec.exists():
+            for file in libexec.iterdir():
+                file.chmod(0o755)
     if result is not None and ok:
         result.put(('MAFFT', ok))
     return ok, str(home_mafft)
