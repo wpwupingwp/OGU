7958e894495a2a578a8d8b1672a605186a4f550c
Ping Wu <wuping@sicnu.edu.cn>
2022-05-11 14:54:41 +0800

    clean out before test

--- all_test.sh
+++ all_test.sh
@@ -1,8 +1,9 @@
 # mac
-seq_n=100
+rm -rf rbcL_Poaceae Rosa_ITS Lamiaceae_cp zea_mays Oryza_cp
+seq_n=101
 python=python3
 $python -m BarcodeFinder.gb2fasta -gene rbcL -taxon Poaceae -out rbcL_Poaceae -seq_n $seq_n
 $python -m BarcodeFinder.gb2fasta -query internal transcribed spacer -taxon Rosa -out Rosa_ITS -uniq no -seq_n $seq_n
-$python -m BarcodeFinder -og cp -refseq -taxon Lamiaceae -out Lamiaceae_cp -seq_n $seq_n
+$python -m BarcodeFinder -og cp -refseq yes -taxon Lamiaceae -out Lamiaceae_cp -seq_n $seq_n
 $python -m BarcodeFinder -taxon Zea_mays -min_len 100 -max_len 3000 -out Zea_mays -primer -seq_n $seq_n
 $python -m BarcodeFinder -taxon Oryza -og mt -min_len 50000 -max_len 200000 -uniq longest -out Oryza_cp -refseq yes -primer -seq_n $seq_n
