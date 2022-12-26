363ac0f05510fd1c505e1e62d66dbcd129ed5044
Ping Wu <wpwupingwp@outlook.com>
2022-12-26 19:27:11 +0800

    clean scrolltext from PAGE

--- ui/ui.py
+++ ui/ui.py
@@ -1183,55 +1183,6 @@ class Primer:
         self.run_b.configure(text='''Run''')
 
 
-class LOG:
-    def __init__(self, top=None):
-        '''This class configures and populates the toplevel window.
-           top is the toplevel containing window.'''
-        _bgcolor = '#edf0f3'  # Closest X11 color: 'gray94'
-        _fgcolor = '#000000'  # X11 color: 'black'
-        _compcolor = '#d9d9d9'  # X11 color: 'gray85'
-        _ana1color = '#d9d9d9'  # X11 color: 'gray85'
-        _ana2color = '#ececec'  # Closest X11 color: 'gray92'
-        _tabfg1 = 'black'
-        _tabfg2 = 'black'
-        _tabbg1 = 'grey75'
-        _tabbg2 = 'grey89'
-        _bgmode = 'light'
-        self.style = ttk.Style()
-        if sys.platform == "win32":
-            self.style.theme_use('winnative')
-        self.style.configure('.', background=_bgcolor)
-        self.style.configure('.', foreground=_fgcolor)
-        self.style.map('.', background=[('selected', _compcolor),
-                                        ('active', _ana2color)])
-
-        top.geometry("600x400+2532+335")
-        top.minsize(72, 15)
-        top.maxsize(3648, 1052)
-        top.resizable(1, 1)
-        top.title("Running...")
-        top.configure(background="#edf0f3")
-        top.configure(highlightbackground="#d9d9d9")
-        top.configure(highlightcolor="black")
-
-        self.top = top
-
-        self.Scrolledtext1 = ScrolledText(self.top)
-        self.Scrolledtext1.place(relx=0.0, rely=0.0, relheight=1.0,
-                                 relwidth=1.0)
-
-        self.Scrolledtext1.configure(background="white")
-        self.Scrolledtext1.configure(font="TkDefaultFont")
-        self.Scrolledtext1.configure(foreground="black")
-        self.Scrolledtext1.configure(highlightbackground="#edf0f3")
-        self.Scrolledtext1.configure(highlightcolor="black")
-        self.Scrolledtext1.configure(insertbackground="black")
-        self.Scrolledtext1.configure(insertborderwidth="3")
-        self.Scrolledtext1.configure(selectbackground="#c4c4c4")
-        self.Scrolledtext1.configure(selectforeground="black")
-        self.Scrolledtext1.configure(wrap="none")
-
-
 class ToolTip(tk.Toplevel):
     """ Provides a ToolTip widget for Tkinter. """
 
