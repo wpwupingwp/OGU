2f9e555fb0e8a70beb4c58eb33e1ca6b2f7cead8
Ping Wu <wpwupingwp@outlook.com>
2022-12-28 16:12:42 +0800

    move init_lineage to utils

--- BarcodeFinder/utils.py
+++ BarcodeFinder/utils.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 import re
 import logging
 import platform
@@ -16,9 +15,11 @@ from sys import argv, version_info
 from threading import Thread
 from urllib.parse import quote
 from urllib.request import urlopen
+from zipfile import ZipFile
+from pkg_resources import resource_filename
 
 from Bio.Seq import Seq
-from BarcodeFinder.global_vars import log, FMT, DATEFMT
+from BarcodeFinder.global_vars import log, name, FMT, DATEFMT
 
 
 # hosting in free AWS S3 server
@@ -614,6 +615,78 @@ def parse_blast_tab(filename):
             else:
                 query.append(BlastResult(line))
     pass
+
+
+def download_taxon(data_folder) -> Path:
+    zip_file = data_folder / 'taxdmp.zip'
+    if zip_file.exists():
+        pass
+    else:
+        url = 'https://ftp.ncbi.nih.gov/pub/taxonomy/taxdmp.zip'
+        with urlopen(url) as response, zip_file.open('wb') as out_file:
+            data = response.read()
+            out_file.write(data)
+    return zip_file
+
+
+def init_lineage():
+    """
+    Only called by setup.py
+    """
+    global name
+    data_folder = Path(
+        resource_filename(name, 'data/button1.png')).absolute().parent
+    zip_file = download_taxon(data_folder)
+    with ZipFile(zip_file, 'r') as dumpfile:
+        dumpfile.extract('names.dmp', path='.')
+        dumpfile.extract('nodes.dmp', path='.')
+    id_name = {}
+    id_rank = {}
+    superkingdoms = []
+    kingdoms = []
+    phyla = []
+    classes = []
+    animal_orders = []
+    with open('names.dmp', 'r') as names:
+        for _ in names:
+            line = _.split(sep='|')
+            taxon_id = line[0].strip()
+            name = line[1].strip()
+            name_type = line[3].strip()
+            if name_type == 'scientific name':
+                id_name[taxon_id] = name
+    with open('nodes.dmp', 'r') as nodes:
+        for _ in nodes:
+            line = _.split(sep='|')
+            taxon_id = line[0].strip()
+            rank = line[2].strip()
+            id_rank[taxon_id] = rank
+    for taxon_id in id_name:
+        rank_name = id_rank[taxon_id]
+        taxon_name = id_name[taxon_id]
+        if rank_name == 'superkingdom':
+            superkingdoms.append(taxon_name)
+        elif rank_name == 'kingdom':
+            kingdoms.append(taxon_name)
+        elif rank_name == 'phylum':
+            phyla.append(taxon_name)
+        elif rank_name == 'class':
+            classes.append(taxon_name)
+        elif rank_name == 'order':
+            if not taxon_name.startswith('ales'):
+                animal_orders.append(taxon_name)
+
+    with open(data_folder / 'superkingdoms.csv', 'w') as out:
+        out.write(','.join(superkingdoms))
+    with open(data_folder / 'kingdoms.csv', 'w') as out:
+        out.write(','.join(kingdoms))
+    with open(data_folder / 'phyla.csv', 'w') as out:
+        out.write(','.join(phyla))
+    with open(data_folder / 'classes.csv', 'w') as out:
+        out.write(','.join(classes))
+    with open(data_folder / 'animal_orders.csv', 'w') as out:
+        out.write(','.join(animal_orders))
+    return
 
 
 def codon_usage(alignment):
