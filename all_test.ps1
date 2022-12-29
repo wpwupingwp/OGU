9ebc737e9a7f80f74e485bc23636626822557104
Ping Wu <wpwupingwp@outlook.com>
2022-12-29 12:49:25 +0800

    update test

--- all_test.ps1
+++ all_test.ps1
@@ -1,15 +1,17 @@
 # mac
+&venv/Scripts/Activate.ps1
 $folder="rbcL_Poaceae","Rosa_ITS","Lamiaceae_cp","zea_mays","Oryza_cp"
 foreach ($f in $folder)
 {
     Remove-Item -Recurse -Force R:\test\$f
 }
-New-Item -p R:\test
-$seq_n="101"
+New-Item R:\test -ItemType Directory
+$count="101"
 $python="python"
 $out="R:\test\"
-&$python -m BarcodeFinder.gb2fasta -gene rbcL -taxon Poaceae -out ${out}rbcL_Poaceae -seq_n $seq_n
+&$python -m BarcodeFinder.gb2fasta -gene rbcL -taxon Poaceae -out ${out}rbcL_Poaceae -count ${count}
 &$python -m BarcodeFinder.gb2fasta -query "internal transcribed spacer" -taxon Rosa -out ${out}Rosa_ITS -uniq no -seq_n $seq_n
 &$python -m BarcodeFinder -og cp -refseq yes -taxon Lamiaceae -out ${out}Lamiaceae_cp -seq_n $seq_n
 &$python -m BarcodeFinder -taxon Zea_mays -min_len 100 -max_len 3000 -out ${out}Zea_mays -primer -seq_n $seq_n
 &$python -m BarcodeFinder -taxon Oryza -og mt -min_len 50000 -max_len 200000 -uniq longest -out ${out}Oryza_cp -refseq yes -primer -seq_n $seq_n
+&deactivate
