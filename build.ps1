995c0a36ad00ec8b5e4514855b01848f09d2de63
Ping Wu <wpwupingwp@outlook.com>
2023-01-07 17:02:21 +0800

    build ok should add primer3/src

--- build.ps1
+++ build.ps1
@@ -3,7 +3,10 @@ rm -Force -Recurse build
 rm -Force -Recurse bf.spec
 rm -Force -Recurse *.egg-info
 venv/Scripts/Activate.ps1
-&pyinstaller BarcodeFinder/__main__.py -c -F -n bf --add-binary 'E:\Linux\barcodefinder\BarcodeFinder\data;data'
+&pyinstaller BarcodeFinder/__main__.py -c -F -n bf `
+--add-data '.\BarcodeFinder\data\*.csv;.\BarcodeFinder\data' `
+--add-data '.\BarcodeFinder\data\*.png;.\BarcodeFinder\data' `
+--add-data '.\venv\Lib\site-packages\primer3\src;.\primer3\src'
 &deactivate
 &dist/bf.exe
 &python -m build -n --wheel
