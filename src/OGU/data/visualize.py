d7a2e268f76fdc23530239060f9d223b3e2b3772
Ping Wu <wpwupingwp@outlook.com>
2024-09-25 23:30:56 +0800

    merge visualize cp and mt

--- src/OGU/data/visualize.py
+++ src/OGU/data/visualize.py
@@ -4,6 +4,7 @@ import re
 from collections import defaultdict
 from pathlib import Path
 
+import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib.patches import Patch
 from pycirclize import Circos
@@ -11,23 +12,22 @@ from pycirclize.parser import Genbank
 
 from OGU.global_vars import log
 
-count_threshold = 100
 # Evaluation.csv -> xlsx -> first column Type, second column name
-# total length - ir length
-irb_start = 131597
-parts = {'LSC': 86684, 'IRa': 25339, 'SSC': 18482, 'IRb': 25339}
+features = ('CDS', 'intron', 'tRNA', 'rRNA', 'spacer')
 feature_colors = '#FFD93D,#DDDDDD,#4D96FF,#9D96FF,#6BCB77'.split(',')
 part_color = '#6BCB77,#4D96FF,#6BCB77,#FF6B6B'.split(',')
 # yellow,yellow/gray,blue,blue,green?
 track_colors = list(reversed('#F7931B,#66B3FF,#338CFF,#3F51B5,#FB9883,#CC5500,'
                              '#FF6347,#36454F,#778877'.split(',')))
-features = ('CDS', 'intron', 'tRNA', 'rRNA', 'spacer')
+track_colors2 = ('#F7931B,#66B3FF,#338CFF,#3F51B5,#FB9883,#CC5500,#FF6347,'
+                 '#36454F,#778877'.split(','))
 colname_text = (('Tree_Res', 'Tree resolution'), ('PD', 'PD'),
                 ('PD_stem', 'PD-stem'), ('PD_terminal', 'PD-terminal'),
                 ('Observed_Res', 'Observed resolution'), ('Pi', 'Pi'),
                 ('Entropy', 'Shannon index'),
                 ('Total_GC', 'GC ratio'),
                 ('Gap_Ratio', 'Gap ratio'))
+
 
 class MyRadius:
     def __init__(self):
@@ -44,11 +44,12 @@ def parse_args(arg_list=None):
     arg = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description=visualize_main.__doc__)
-    arg.add_argument('-input', help='Evaluation result tabel, csv file')
-    arg.add_argument('-ref', help='reference genome genbank file')
+    arg.add_argument('-input_csv', help='Evaluation result tabel, csv file')
+    arg.add_argument('-ref_gb', help='reference genome genbank file')
     arg.add_argument('-taxa', help='Taxa name, recommend use family name')
     arg.add_argument('-type', choices=('cp', 'mt'), dest='og_type',
-                     help='Type of organelle (mt:mitochondria or cp:plastid')
+                     help='Type of organelle (mt:mitochondria or '
+                          'cp:plastid/chloroplast')
     arg.add_argument('n_seqs', type=int, dest='count_threshold', default=10,
                      help='Minimum number of sequences per gene/fragment')
     # use tobacco as default
@@ -65,7 +66,7 @@ def parse_args(arg_list=None):
         return arg.parse_known_args(arg_list)
 
 
-def init_args(arg):
+def init_arg(arg):
     # since visualize function is not strictly bound with other modules, only
     # check input here
     if arg.input is None:
@@ -96,12 +97,11 @@ def init_args(arg):
     arg.out = Path(arg.out).absolute()
     if arg.out.exists():
         log.warning(f'{arg.out} already exists. Overwriting.')
+    # tobacco plastid genome
+    arg.parts = {'LSC': arg.lsc, 'IRb': arg.ir, 'SSC': arg.ssc, 'IRa': arg.ir}
+    # total length - ir length
+    arg.irb_start = arg.lsc + arg.ir + arg.ssc
     return arg
-
-
-def get_ref_gb(taxa: str, og_type: str) -> Path:
-    log.info(f'Try to get reference of {taxa} {og_type} genome from Genbank')
-    pass
 
 
 def get_name(old):
@@ -112,30 +112,29 @@ def get_name2(old):
     return old.split('-')[0]
 
 
+def get_ref_gb(taxa: str, og_type: str) -> Path:
+    log.info(f'Try to get reference of {taxa} {og_type} genome from Genbank')
+    pass
+
+
 def percent_to_float(old: str) -> float:
     return float(old.rstrip('%')) / 100
 
 
-def read_csv(filename: Path) -> dict:
-    # Pandas is too heavy, use csv to implement pandas.read_csv
-    columns = defaultdict(list)
-    with open(filename, newline='') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            for (k, v) in row.items():
-                columns[k].append(v)
-    return columns
+# def read_csv(filename: Path) -> dict:
+#     # Pandas is too heavy, use csv to implement pandas.read_csv
+#     columns = defaultdict(list)
+#     with open(filename, newline='') as f:
+#         reader = csv.DictReader(f)
+#         for row in reader:
+#             for (k, v) in row.items():
+#                 columns[k].append(v)
+#     return columns
 
 
-def x():
-    #%%
-    # read data
-    # evaluation result table, remove abnormal genes
-    filename = 'Evaluation.csv'
-    # reference organelle genome genbank file, generated from OGU.gb2fasta
-    gb_file = r'tobacco_extend.gb'
-    gb = Genbank(gb_file)
-    data_raw = pd.read_csv(filename)
+def preprocess_data(csv_file: Path, count_threshold) -> (pd.DataFrame, set):
+    data_raw = pd.read_csv(csv_file)
+    # spacer-atpB-rbcL to ('spacer', 'atpB-rbcL')
     data_raw['Name'] = data_raw.Loci.apply(get_name)
     data_raw['Loci'] = data_raw.Loci.apply(get_name2)
     data_raw['Gap_Ratio'] = data_raw['Gap_Ratio'].apply(percent_to_float)
@@ -143,41 +142,79 @@ def x():
     data_raw['Total_GC'] = data_raw['Total_GC'].apply(percent_to_float)
     data_raw['Observed_Res'] = data_raw['Observed_Res'].apply(percent_to_float)
     data_raw2 = data_raw[data_raw.Samples >= count_threshold]
-    data = data_raw2[data_raw2.Loci != 'gene']
-    data_names_raw = data.Name.tolist()
+    # gene is duplicated with CDS/rna
+    data_raw3 = data_raw2[data_raw2.Loci != 'gene']
+    data_names_raw = data_raw3.Name.tolist()
     data_names = list()
     for i in data_names_raw:
         new = re.sub('trn(.|fM)_...', r'trn\1', i)
         data_names.append(new)
+    names_set = set(data_names)
+    return data_raw3, names_set
 
-    #%%
+
+def visualize_main(arg_str=None):
+    log.info('Running visualize module...')
+    if arg_str is None:
+        arg, other_args2 = parse_args()
+    else:
+        arg, other_args2 = parse_args(arg_str.split(' '))
+    arg = init_arg(arg)
+    if arg is None:
+        log.info('Quit visualize module.')
+        return None, other_args2
+
+    def draw_bar(track, x_, y_, w_, text):
+        max_y = max(y_)
+        color = track_colors.pop()
+        # ec='black', lw=0.5
+        for x, y, w in zip(x_, y_, w_):
+            # bar need x[list], y[list]
+            # x should adjust by width
+            track.bar([x - w / 2], [y], width=w * 0.95, color=color,
+                      vmin=0, vmax=max_y)
+        track.text(text, t_pos, size=8, color=color, adjust_rotation=True)
+
+    # evaluation result table, remove abnormal genes
+    long_label = 20
+    # reference organelle genome genbank file, generated from OGU.gb2fasta
+    gb = Genbank(arg.ref_gb)
+    if arg.og_type == 'cp':
+        t_pos = gb.range_size - arg.parts['IRb'] / 2
+    data_raw3, data_names_set = preprocess_data(arg.input, arg.count_threshold)
+
     r = MyRadius()
 
-    #%%
-    fig = plt.figure(figsize=(10, 10))
-    # circos = Circos(sectors, space=0.5)
-    # genome gb file as template, no extra treatmetn
+    fig_ = plt.figure(figsize=(10, 10))
+    # genome gb file as template, no extra treatment
+    if arg.og_type == 'cp':
+        circle_start = -260
+        circle_end = circle_start + 360
+    else:
+        circle_start = 0
+        circle_end = 360
     circos = Circos(sectors={gb.name: gb.range_size},
                     sector2clockwise={gb.name: False},
-                    start=-260, end=-260 + 360)
+                    start=circle_start, end=circle_end)
     sector = circos.sectors[0]
 
     r1 = r.get()
     feature_track = sector.add_track(r1)
-    feature_track.axis(fc="#FFFFFF", ec="none")
+    feature_track.axis(fc='#FFFFFF', ec='none')
     gene_labels = []
     gene_list = []
     last_gene = ''
     gene_set = set()
     ir_pos_list = set()
-    for feat in gb.extract_features("gene"):
+    for feat in gb.extract_features('gene'):
         start, end = int(str(feat.location.start)), int(str(feat.location.end))
         pos = (start + end) / 2
-        label = feat.qualifiers.get("gene", ["??"])[0]
-        if label == "" or label.startswith("hypothetical") or label == last_gene:
+        label = feat.qualifiers.get('gene', ['??'])[0]
+        if (label == '' or label.startswith('hypothetical')
+                or label == last_gene):
             continue
-        if len(label) > 20:
-            label = label[:20] + "..."
+        if len(label) > long_label:
+            label = label[:long_label] + "..."
         last_gene = label
         gene_list.append(pos)
         gene_labels.append(label)
@@ -189,10 +226,10 @@ def x():
     feature_track.xticks(
         gene_list,
         gene_labels,
-        label_orientation="vertical",
+        label_orientation='vertical',
         show_bottom_line=True,
         label_size=6,
-        #line_kws=dict(ec="white"),
+        # line_kws=dict(ec="white"),
     )
     # extract names
     pos_list, labels = [], []
@@ -200,7 +237,7 @@ def x():
     for feature, color in zip(features, feature_colors):
         feature_track.genomic_features(
             gb.extract_features(feature),
-            plotstyle="box",
+            plotstyle='box',
             r_lim=r1,
             fc=color, alpha=1, lw=0.1, ec='black')
         for f in gb.extract_features(feature):
@@ -213,20 +250,20 @@ def x():
                 if label == last_name:
                     continue
             else:
-                label = f.qualifiers['upstream'][0] + '-' + f.qualifiers['downstream'][0]
+                label = (f.qualifiers['upstream'][0] + '-'
+                         + f.qualifiers['downstream'][0])
             last_name = label
             pos_list.append(pos)
             labels.append(label)
 
     labels_set = set(labels)
-    data_names_set = set(data_names)
-    #a_b = sorted(list(data_names_set-labels_set))
-    #print('data_names_set-labels_set', a_b, len(a_b))
-    #print('\n'*3)
-    #b_a = sorted(list(labels_set-data_names_set))
-    #print('labels_set-data_names_set', b_a, len(b_a))
+    # a_b = sorted(list(data_names_set-labels_set))
+    # print('data_names_set-labels_set', a_b, len(a_b))
+    # print('\n'*3)
+    # b_a = sorted(list(labels_set-data_names_set))
+    # print('labels_set-data_names_set', b_a, len(b_a))
     intersection = data_names_set & labels_set
-    clean_data = data.query('Name in @intersection')
+    clean_data = data_raw3.query('Name in @intersection')
     label_pos = list()
     label_pos_dict = dict()
     ir_second_label_pos = list()
@@ -249,55 +286,52 @@ def x():
         w = clean_pos[i] - clean_pos[i - 1]
         widths.append(w)
 
-    t_pos = gb.range_size - parts['IRb'] / 2
-
-
-    def draw_bar(track, x_, y_, w_, text):
-        max_y = max(y_)
-        color = track_colors.pop()
-        for x, y, w in zip(x_, y_, w_):
-            # bar need x[list], y[list]
-            # x should adjust by width
-            track.bar([x - w / 2], [y], width=w * 0.95, color=color,
-                      vmin=0, vmax=max_y)
-        track.text(text, t_pos, size=8, color=color, adjust_rotation=True)
-
-        # ec='black', lw=0.5
-
-
+    # print(clean_data[['Name','Position','Loci']].head, clean_data.shape,
+    # len(clean_pos))
+    # print(clean_data.Name.tolist(), clean_pos)
+    # print(ir_second_label_pos)
     for col, text in colname_text:
         track = sector.add_track(r.get(), r_pad_ratio=0.1)
         track.axis()
         data = clean_data[col].tolist()
         draw_bar(track, clean_pos, data, widths, text)
 
-    part_track = sector.add_track(r.get(True))
-    start = 0
-    for part in parts.keys():
-        part_track.rect(start, start + parts[part], fc=part_color.pop(), lw=0.5)
-        part_track.text(part, (start + start + parts[part]) / 2,
-                        color='white', size=8)
-        start = start + parts[part] + 1
-    circos.link((gb.name, parts['LSC'], parts['LSC'] + parts['IRa']),
-                (gb.name, gb.range_size - parts['IRb'], gb.range_size),
-                color='green', alpha=0.3)
-
-    # print(clean_data.columns)
+    if arg.og_type == 'cp':
+        # draw quadripartite structure for plastid
+        gb_size = (arg.parts['LSC'] + arg.parts['IRa'] + arg.parts['SSC'] +
+                   arg.parts['IRb'])
+        part_track = sector.add_track(r.get(True))
+        start = 0
+        for part in arg.parts.keys():
+            part_track.rect(start, start + arg.parts[part], fc=part_color.pop(),
+                            lw=0.5)
+            part_track.text(part, (start + start + arg.parts[part]) / 2,
+                            color='white', size=8)
+            start = start + arg.parts[part] + 1
+        circos.link(
+            (gb.name, arg.parts['LSC'], arg.parts['LSC'] + arg.parts['IRa']),
+            (gb.name, gb_size - arg.parts['IRb'], gb_size),
+            color='green', alpha=0.3)
 
     fig = circos.plotfig()
 
+    # add legends
     rect_handles = []
-    rect_colors = feature_colors
-    for name, color in zip(features, rect_colors):
+    for name, color in zip(features, feature_colors):
         rect_handles.append(Patch(color=color, label=name))
-    _ = circos.ax.legend(handles=rect_handles, # bbox_to_anchor=(0.5, 0.5),
-                         loc='lower right', fontsize=8, title="Types", ncol=2)
-    #%%
-    fig.savefig('out.pdf')
-
-
-def visualize_main():
-    pass
+    if arg.og_type == 'mt':
+        rect_handles.append(Patch(color='#999999', label='D-loop'))
+        for name, color in zip(colname_text, track_colors2):
+            rect_handles.append(Patch(color=color, label=name[1]))
+        _ = circos.ax.legend(handles=rect_handles, bbox_to_anchor=(1, 0),
+                             loc='best', fontsize=8, title='Types', ncol=3)
+    else:
+        _ = circos.ax.legend(handles=rect_handles,
+                             # bbox_to_anchor=(0.5, 0.5),
+                             loc='lower right', fontsize=8, title='Types',
+                             ncol=2)
+    fig.savefig(arg.out)
+    return arg.out
 
 
 if __name__ == '__main__':
